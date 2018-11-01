5d582d4cc10dcc42dbd2581c96746c5e47d1579d
rxliuli <rxliuli@gmail.com>
2018-11-01 22:57:19 +0800

    更新了生成唯一标识字符串的函数名与文件名对应

--- src/obj/uniqueSymbolString.test.js
+++ src/obj/uniqueSymbolString.test.js
@@ -5,15 +5,15 @@ test('测试生成唯一标识字符串的功能', () => {
   const str = '1'
   const symbol = Symbol(1)
 
-  expect(rxobj.uniqueSymbol(i)).not.toBe(rxobj.uniqueSymbol(str))
-  expect(rxobj.uniqueSymbol(str)).not.toBe(
-    rxobj.uniqueSymbol(symbol)
+  expect(rxobj.uniqueSymbolString(i)).not.toBe(rxobj.uniqueSymbolString(str))
+  expect(rxobj.uniqueSymbolString(str)).not.toBe(
+    rxobj.uniqueSymbolString(symbol)
   )
-  expect(rxobj.uniqueSymbol(i)).not.toBe(rxobj.uniqueSymbol(symbol))
-  expect(rxobj.uniqueSymbol(i)).toBe(rxobj.uniqueSymbol(i))
-  expect(rxobj.uniqueSymbol(str)).toBe(rxobj.uniqueSymbol(str))
+  expect(rxobj.uniqueSymbolString(i)).not.toBe(rxobj.uniqueSymbolString(symbol))
+  expect(rxobj.uniqueSymbolString(i)).toBe(rxobj.uniqueSymbolString(i))
+  expect(rxobj.uniqueSymbolString(str)).toBe(rxobj.uniqueSymbolString(str))
   // 可以看到 symbol 类型即便是同一个也会生成不同的字符串
-  expect(rxobj.uniqueSymbol(symbol)).not.toBe(
-    rxobj.uniqueSymbol(symbol)
+  expect(rxobj.uniqueSymbolString(symbol)).not.toBe(
+    rxobj.uniqueSymbolString(symbol)
   )
 })

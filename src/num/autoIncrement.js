5e9e8f2b0191813ea8569c143a74135ab88ad04e
rxliuli <rxliuli@gmail.com>
2018-11-01 22:39:25 +0800

    添加自动获取自增序列值的函数

---
+++ src/num/autoIncrement.js
@@ -0,0 +1,15 @@
+/**
+ * 一个自动增长的 Number 序列，从 1 开始
+ */
+module.exports = (() => {
+  function* gennerateId() {
+    let i = 1
+    for (;;) {
+      yield i++
+    }
+  }
+  const generatorId = gennerateId()
+  return function() {
+    return generatorId.next().value
+  }
+})()
---
+++ src/num/index.js
@@ -0,0 +1,3 @@
+module.exports = {
+  autoIncrement: require('./autoIncrement')
+}

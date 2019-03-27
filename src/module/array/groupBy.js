1f20fe54e56f7cc235d7a927c6d2f3bce56a45d6
rxliuli <rxliuli@gmail.com>
2019-03-27 15:17:51 +0800

    更新函数 [groupBy], 使用 Map 而非 Object 作为结果

--- src/module/array/groupBy.js
+++ src/module/array/groupBy.js
@@ -1,17 +1,21 @@
 /**
  * js 数组按照某个条件进行分组
- * @param {Array} arr 要进行分组的数组
- * @param {Function} {fn} 元素分组的方法，默认使用 {@link JSON.stringify()}
- * @returns {Object<String, Array>} 对象 -> 数组映射对象
+ *
+ * @param {Array<V>} arr 要进行分组的数组
+ * @param {Function<V,K>} fn 元素分组的方法
+ * @returns {Map<K,V>} 对象 -> 数组映射对象
  */
-function groupBy (arr, fn = item => JSON.stringify(item)) {
+function groupBy (arr, fn) {
   // 将元素按照分组条件进行分组得到一个 条件 -> 数组 的对象
-  return arr.reduce((obj, item) => {
+  return arr.reduce((res, item) => {
     const name = fn(item)
     // 如果已经有这个键了就直接追加, 否则先将之赋值为 [] 再追加元素
-    ;(obj[name] || (obj[name] = [])).push(item)
-    return obj
-  }, {})
+    if (!res.has(name)) {
+      res.set(name, [])
+    }
+    res.get(name).push(item)
+    return res
+  }, new Map())
 }
 
 export default groupBy

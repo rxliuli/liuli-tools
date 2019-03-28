7e344fdc4582f085542c8201b578efc084d31979
rxliuli <rxliuli@gmail.com>
2019-03-28 11:52:01 +0800

    更新 readLocal, 添加 encoding 参数

--- src/module/ajax/readLocal.js
+++ src/module/ajax/readLocal.js
@@ -1,11 +1,12 @@
 /**
  * 读取本地浏览器选择的文件
  * @param {File} file 选择的文件
- * @param { readLocal.DataURL | readLocal.Text | readLocal.BinaryString | readLocal.ArrayBuffer } [type=readLocal.DataURL] 读取的类型，默认为
+ * @param { readLocal.DataURL | readLocal.Text | readLocal.BinaryString | readLocal.ArrayBuffer } [type=readLocal.DataURL] 读取的类型，默认按照二进制 url 读取
+ * @param {String} encoding 读取的编码格式，默认为 UTF-8
  * @returns {Promise} 返回了读取到的内容（异步）
  */
 const readLocal = (() => {
-  const result = (file, { type = 'readAsDataURL' } = {}) =>
+  const result = (file, { type = 'readAsDataURL', encoding = 'UTF-8' } = {}) =>
     new Promise((resolve, reject) => {
       if (!file) {
         reject(new Error('file not exists'))
@@ -17,7 +18,7 @@ const readLocal = (() => {
       fr.onerror = error => {
         reject(error)
       }
-      fr[type](file)
+      fr[type](file, encoding)
     })
   result.DataURL = 'readAsDataURL'
   result.Text = 'readAsText'

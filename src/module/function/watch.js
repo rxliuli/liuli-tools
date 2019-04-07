f9d817356b4b2ad127be9dac3b1f8f78cc836eef
rxliuli <rxliuli@gmail.com>
2019-04-07 14:55:14 +0800

    添加注释 [监视指定函数返回值的变化]

--- src/module/function/watch.js
+++ src/module/function/watch.js
@@ -6,7 +6,7 @@ import { safeExec } from './safeExec'
  * @param {Function} fn 需要监视的函数
  * @param {Function} callback 回调函数
  * @param {Number} [interval=100] 每次检查的间隔时间，默认为 100ms
- * @returns {}
+ * @returns {Function} 关闭这个监视函数
  */
 export function watch (fn, callback, interval = 100) {
   let oldVal = safeExec(fn)

44d5fba4f1a5a6f8a8dfc1ef7e1743ac12cf6a7e
rxliuli <rxliuli@gmail.com>
2019-04-16 10:25:35 +0800

    为函数 [autoIncrement] 添加注释

--- src/module/number/autoIncrement.js
+++ src/module/number/autoIncrement.js
@@ -1,16 +1,25 @@
 // @ts-check
-
+import { onec } from './../function/onec'
+/**
+ * 使用 Generator 实现一个从 0 开始的自增序列
+ */
 function * autoIncrementGenerator () {
   for (let i = 0; ; i++) {
+    /**
+     * @returns {Number} 每次获取都返回循环中的当前迭代变量，然后暂停于此处
+     */
     yield i
   }
 }
+/**
+ * 包装 {@link autoIncrementGenerator} 为只能调用一次的函数
+ */
+const generator = onec(autoIncrementGenerator)
 
-const generator = autoIncrementGenerator()
 /**
  * 获取自增长序列的最新值
  * @returns {Number} 最新值
  */
 export function autoIncrement () {
-  return generator.next().value
+  return generator().next().value
 }

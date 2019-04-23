29bffdf61088407257b8957e383993c9f76806c8
rxliuli <rxliuli@gmail.com>
2019-04-23 20:23:58 +0800

    完善类 [DateFormatter] 的测试用例

--- src/index.js
+++ src/index.js
@@ -85,6 +85,7 @@ import {
 import { deepFreeze } from './module/obj/deepFreeze'
 import { deepProxy } from './module/obj/deepProxy'
 import { curry } from './module/function/curry'
+import { DateFormatter } from './module/date/DateFormatter'
 
 /**
  * 全局导出的对象，用于浏览器中使用的全局变量 rx
@@ -114,6 +115,7 @@ export {
   dateFormat,
   dateParse,
   dateConstants,
+  DateFormatter,
   dateEnhance,
   dateBetween,
   strToDate,
--- src/module/date/DateFormatter.js
+++ src/module/date/DateFormatter.js
@@ -33,14 +33,27 @@ export class DateFormatter {
     return dateParse(str, this.fmt)
   }
   /**
-   * 日期时间转换
+   * 将日期时间字符串转换为前端指定格式的字符串
+   * 主要适用场景是前端接收到后端的日期时间一般是一个字符串，然而需要自定义格式的时候还必须先创建 {@link Date} 对象才能格式化，略微繁琐，故使用该函数
    * @param {String} str 字符串
-   * @param {String} fmt 日期时间格式
-   * @param {String} parseFmt 解析的日期时间格式
+   * @param {String} [parseFmt=undefined] 解析的日期时间格式。默认直接使用 {@link new Date()} 创建
    * @returns {String} 转换后得到的字符串
    */
-  static convert (str, fmt, parseFmt) {
+  strFormat (str, parseFmt) {
     const date = parseFmt ? dateParse(str, parseFmt) : new Date(str)
-    return dateFormat(date, fmt)
+    return dateFormat(date, this.fmt)
   }
 }
+
+/**
+ * 日期格式化器
+ */
+DateFormatter.dateFormatter = new DateFormatter('yyyy-MM-dd')
+/**
+ * 时间格式化器
+ */
+DateFormatter.timeFormatter = new DateFormatter('hh:mm:ss')
+/**
+ * 日期时间格式化器
+ */
+DateFormatter.dateTimeFormatter = new DateFormatter('yyyy-MM-dd hh:mm:ss')

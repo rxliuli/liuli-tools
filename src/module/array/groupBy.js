1bbce2014b5b08cd5719af235f4124371a3843e6
rxliuli <rxliuli@gmail.com>
2019-05-04 10:59:25 +0800

    增强函数 [groupBy], 允许对分组后的结果进行后处理

--- src/module/array/groupBy.js
+++ src/module/array/groupBy.js
@@ -2,18 +2,33 @@
  * js 数组按照某个条件进行分组
  *
  * @param {Array<Object>} arr 要进行分组的数组
- * @param {Function} fn 元素分组的方法
+ * @param {Function} kFn 元素分组的唯一标识函数
+ * @param {Function} [vFn] 元素分组的值处理的函数。第一个参数是累计值，第二个参数是当前正在迭代的元素，如果你使用过 {@link Array#reduce} 函数的话应该对此很熟悉
+ * @param {Function} [init=[]] 每个分组的产生初始值的函数。类似于 reduce 的初始值，但它是一个函数，避免初始值在所有分组中进行累加。
  * @returns {Map<Object,Object>} 对象 -> 数组映射对象
  */
-export function groupBy (arr, fn) {
+export function groupBy (
+  arr,
+  kFn,
+  /**
+   * 默认的值处理函数
+   * @param {Map} res 最终 map 集合
+   * @param {Object} item 当前迭代的元素
+   */
+  vFn = (res, item) => {
+    res.push(item)
+    return res
+  },
+  init = () => []
+) {
   // 将元素按照分组条件进行分组得到一个 条件 -> 数组 的对象
   return arr.reduce((res, item) => {
-    const name = fn(item)
+    const k = kFn(item)
     // 如果已经有这个键了就直接追加, 否则先将之赋值为 [] 再追加元素
-    if (!res.has(name)) {
-      res.set(name, [])
+    if (!res.has(k)) {
+      res.set(k, init())
     }
-    res.get(name).push(item)
+    res.set(k, vFn(res.get(k), item))
     return res
   }, new Map())
 }
--- src/module/array/groupBy.test.js
+++ src/module/array/groupBy.test.js
@@ -10,4 +10,41 @@ describe('test groupBy', () => {
       new Map().set(true, [2]).set(false, [1, 3])
     )
   })
+  it('groupBy for eval sum', () => {
+    const arr = [1, 2, 3, 4]
+    // 分组完成之后立即计算总和
+    expect(
+      groupBy(arr, i => i % 2 === 0, (res, i) => (res += i), () => 0)
+    ).toEqual(new Map().set(true, 6).set(false, 4))
+  })
+  it('groupBy for custom vFn', () => {
+    class User {
+      constructor (id, name, sex) {
+        this.id = id
+        this.name = name
+        this.sex = sex
+      }
+    }
+    const users = [
+      new User(1, 'rx', 1),
+      new User(2, '琉璃', 1),
+      new User(3, '楚轩', 2),
+    ]
+    // 分组完成将结果集转为 Map
+    expect(
+      groupBy(
+        users,
+        ({ sex }) => sex % 2 === 0,
+        (res, { id, name }) => {
+          res.set(id, name)
+          return res
+        },
+        () => new Map()
+      )
+    ).toEqual(
+      new Map()
+        .set(false, new Map().set(1, 'rx').set(2, '琉璃'))
+        .set(true, new Map().set(3, '楚轩'))
+    )
+  })
 })

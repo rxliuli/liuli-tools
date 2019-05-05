07c9f0c9396f7841659ddc10ecc6272b1fce5747
rxliuli <rxliuli@gmail.com>
2019-05-05 21:51:11 +0800

    修复注释 [toObject]

--- src/module/array/toObject.js
+++ src/module/array/toObject.js
@@ -1,6 +1,6 @@
 /**
  * 将数组转化为一个 Object 对象
- * @deprecated 已废弃，请使用更好的 @override arrayToMap 替代
+ * @deprecated 已废弃，请使用更好的 {@link arrayToMap} 替代
  * @param {Array.<Object>} arr 需要进行转换的数组
  * @param {Function} kFn 生成对象属性名的函数
  * @param {Function} [vFn] 生成对象属性值的函数，默认为数组中的迭代元素

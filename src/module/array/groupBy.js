170537a481921a6d34e9d525c072e33d95bf5d3b
rxliuli <rxliuli@gmail.com>
2019-05-06 18:42:19 +0800

    修改注释 [groupBy]

--- src/module/array/groupBy.js
+++ src/module/array/groupBy.js
@@ -5,7 +5,7 @@
  * @param {Function} kFn 元素分组的唯一标识函数
  * @param {Function} [vFn] 元素分组的值处理的函数。第一个参数是累计值，第二个参数是当前正在迭代的元素，如果你使用过 {@link Array#reduce} 函数的话应该对此很熟悉
  * @param {Function} [init=[]] 每个分组的产生初始值的函数。类似于 reduce 的初始值，但它是一个函数，避免初始值在所有分组中进行累加。
- * @returns {Map<Object,Object>} 对象 -> 数组映射对象
+ * @returns {Map<Object,Object>} 元素标识 -> 数组映射 Map
  */
 export function groupBy (
   arr,

48d83ab8783973722a806c857eaa3a2f5613df25
rxliuli <rxliuli@gmail.com>
2019-05-08 22:10:54 +0800

    更新函数 [singleModel], 修改为使用 Proxy 实现通用的单例模式

--- src/module/function/singleModel.js
+++ src/module/function/singleModel.js
@@ -1,20 +1,17 @@
 /**
- * 通用的单例模式
+ * 使用 Proxy 实现通用的单例模式
  * @param {Object} clazz 需要包装为单例的类型
  * @returns {Object} 包装后的单例模式类，使用 {@code new} 创建将只在第一次有效
  */
 export function singleModel (clazz) {
   let instance
-  return class SingleClass extends clazz {
-    /**
-     * @param {...Object} args
-     */
-    constructor (...args) {
+  return new Proxy(clazz, {
+    construct (target, args, newTarget) {
       if (instance) {
         return instance
       }
-      super(...args)
-      instance = this
-    }
-  }
+      instance = Reflect.construct(target, args, newTarget)
+      return instance
+    },
+  })
 }

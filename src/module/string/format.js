fe7ceefeb366845421ae962598dc6d7f7b810058
rxliuli <rxliuli@gmail.com>
2019-05-13 17:46:30 +0800

    废弃函数 [fill], 更新 [format] 的替代原生特性的 url

--- src/module/string/format.js
+++ src/module/string/format.js
@@ -4,7 +4,7 @@
  * @param {String} str 要进行格式化的值
  * @param {Object} args 格式化参数值，替换字符串中的 {} 的值
  * @returns {String} 替换完成的字符串
- * @deprecated 已废弃，请使用 ES6 模板字符串 https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/template_strings
+ * @deprecated 已废弃，请使用 ES6 模板字符串 {@url(https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/template_strings)}
  */
 export function format (str, args) {
   if (!args) {

e93b81e1252ef87371192ac99975f2da9506e259
rxliuli <rxliuli@gmail.com>
2019-05-13 18:56:33 +0800

    尝试添加 [Lock] 异步并发限制工具(未完成)

---
+++ src/module/ajax/Locker.js
@@ -0,0 +1,41 @@
+import { wait } from '../function/wait'
+
+/**
+ * 默认的超时时间，可以认为是无限
+ */
+const TimeoutInfinity = () => false
+/**
+ * 创建一个 Lock 对象，用于锁住当前的当前的异步流程
+ */
+export class Locker {
+  constructor ({ limit = 1, timeout = TimeoutInfinity } = {}) {
+    this.limit = limit
+    this.timeout = timeout
+  }
+  /**
+   * 当前是否锁住了
+   */
+  isLocked () {
+    return this.limit <= 0
+  }
+  /**
+   * 添加异步锁
+   * @param {Number} timeout 超时时间
+   */
+  // @ts-ignore
+  async lock (timeout = this.timeout) {
+    if (this.isLocked()) {
+      /**
+       * @type {Number|Function}
+       */
+      await Promise.race([wait(() => !this.isLocked()), wait(timeout)])
+    }
+    this.limit--
+  }
+  /**
+   * 删除异步锁
+   */
+  unlock () {
+    this.limit++
+  }
+}

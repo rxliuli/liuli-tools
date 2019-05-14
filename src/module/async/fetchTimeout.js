ff565f0af3175c5554a3c4227530ba85cb4dde4a
rxliuli <rxliuli@gmail.com>
2019-05-15 00:12:40 +0800

    更新函数 [fetchTimeout], 修复 wait 函数移动到 function 包的问题

--- src/module/async/fetchTimeout.js
+++ src/module/async/fetchTimeout.js
@@ -1,3 +1,5 @@
+import { wait } from '../function/wait'
+
 /**
  * 为 fetch 请求添加超时选项
  * 注：超时选项并非真正意义上的超时即取消请求，请求依旧正常执行完成，但会提前返回 reject 结果
@@ -6,17 +8,5 @@
  * @returns {Promise} 如果超时就提前返回 reject, 否则正常返回 fetch 结果
  */
 export function fetchTimeout (fetchPromise, timeout) {
-  let abortFn = null
-  // 这是一个可以被 reject 的 Promise
-  const abortPromise = new Promise(function (resolve, reject) {
-    abortFn = function () {
-      reject(new Error('abort promise'))
-    }
-  })
-  // 有一个 Promise 完成就立刻结束
-  const abortablePromise = Promise.race([fetchPromise, abortPromise])
-  setTimeout(function () {
-    abortFn()
-  }, timeout)
-  return abortablePromise
+  return Promise.race([fetchPromise, wait(timeout)])
 }

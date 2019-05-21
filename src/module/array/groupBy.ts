0a968d6db82bcb05b46c89f7e8e2fa5cee2e4662
rxliuli <rxliuli@gmail.com>
2019-05-21 18:33:33 +0800

    更新 [groupBy]

--- src/module/array/groupBy.ts
+++ src/module/array/groupBy.ts
@@ -1,5 +1,6 @@
 import { ArrayCallback } from '../interface/ArrayCallback'
 import { convert } from '../interface/convert'
+import { arrayToMap } from './arrayToMap'
 
 /**
  * js 数组按照某个条件进行分组
@@ -10,7 +11,19 @@ import { convert } from '../interface/convert'
  * @param {Function} [init=[]] 每个分组的产生初始值的函数。类似于 reduce 的初始值，但它是一个函数，避免初始值在所有分组中进行累加。
  * @returns {Map<Object,Object>} 元素标识 -> 数组映射 Map
  */
+export function groupBy<T, K>(arr: T[], kFn: ArrayCallback<T, K>): Map<K, T[]>
 export function groupBy<T, K, R>(
+  arr: T[],
+  kFn: ArrayCallback<T, K>,
+  vFn: (res: R[], item: T, index: number, arr: T[]) => R[],
+): Map<K, R[]>
+export function groupBy<T, K, V>(
+  arr: T[],
+  kFn: ArrayCallback<T, K>,
+  vFn: (res: V, item: T, index: number, arr: T[]) => V,
+  init: () => V,
+): Map<K, V>
+export function groupBy<T, K, V = T[]>(
   arr: T[],
   kFn: ArrayCallback<T, K>,
   /**
@@ -18,14 +31,14 @@ export function groupBy<T, K, R>(
    * @param {Map} res 最终 map 集合
    * @param {Object} item 当前迭代的元素
    */
-  vFn: (res: R, item: T, index: number, arr: T[]) => R = convert(
+  vFn: (res: V, item: T, index: number, arr: T[]) => V = convert(
     (res: T[], item: T) => {
       res.push(item)
       return res
     },
   ),
-  init: () => R = convert(() => []),
-): Map<K, R> {
+  init: () => V = convert(() => []),
+): Map<K, V> {
   // 将元素按照分组条件进行分组得到一个 条件 -> 数组 的对象
   return arr.reduce((res, item: T, index: number, arr: T[]) => {
     const k = kFn(item, index, arr)
@@ -33,7 +46,7 @@ export function groupBy<T, K, R>(
     if (!res.has(k)) {
       res.set(k, init())
     }
-    res.set(k, vFn(res.get(k), item, index, arr))
+    res.set(k, vFn(res.get(k)!, item, index, arr))
     return res
-  }, new Map())
+  }, new Map<K, V>())
 }

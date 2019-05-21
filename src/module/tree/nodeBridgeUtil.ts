54b4970a8890fdcc01345beeab2566405b2fcf7e
rxliuli <rxliuli@gmail.com>
2019-05-22 01:04:23 +0800

    重新添加函数 [nodeBridgeUtil]

--- src/module/tree/nodeBridgeUtil.ts
+++ src/module/tree/nodeBridgeUtil.ts
@@ -7,10 +7,11 @@ import { INode } from './INode'
  * 树节点桥接工具类
  * 主要实现了桥接 {@field bridge} {@field bridgeTree} 和 {@field bridgeList} 三个函数，事实上桥接之后再转换相当于做了两遍，但就目前而言暂且只能如此了
  */
+
 export class NodeBridgeUtil {
   /**
    * 桥接对象为标准的树结构
-   * @param [nodeBridge=new NodeBridge()] 桥接对象
+   * @param nodeBridge 桥接对象
    * @returns 代理函数
    */
   public bridge<T>({
@@ -29,7 +30,7 @@ export class NodeBridgeUtil {
   /**
    * 桥接一棵完整的树
    * @param tree 树节点
-   * @param [nodeBridge=new INodeBridge()] 桥接对象
+   * @param nodeBridge 桥接对象
    * @returns 代理后的树对象
    */
   public bridgeTree<T>(tree: T, nodeBridge?: INodeBridge): INode {
@@ -40,7 +41,7 @@ export class NodeBridgeUtil {
   /**
    * 桥接一个树节点列表
    * @param list 树节点列表
-   * @param [nodeBridge=new NodeBridge()] 桥接对象
+   * @param nodeBridge 桥接对象
    * @returns 代理后的树节点列表
    */
   public bridgeList<T>(list: T[], nodeBridge?: INodeBridge): INode[] {

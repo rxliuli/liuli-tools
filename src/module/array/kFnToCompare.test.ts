77de22ba6a206aeb20a34e2eb7a05966679e0a0c
rxliuli <rxliuli@gmail.com>
2019-05-22 01:03:52 +0800

    添加函数 [kFnToCompare]

---
+++ src/module/array/kFnToCompare.test.ts
@@ -0,0 +1,9 @@
+import { kFnToCompare } from './kFnToCompare'
+
+describe('test kFnToCompare', () => {
+  it('simple example', () => {
+    const arr = [1, 3, 2, 4]
+    expect(arr.sort(kFnToCompare(i => i))).toIncludeAllMembers([1, 2, 3, 4])
+    expect(arr.sort(kFnToCompare(i => -i))).toIncludeAllMembers([4, 3, 2, 1])
+  })
+})

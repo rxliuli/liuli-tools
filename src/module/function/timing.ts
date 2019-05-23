a61dfb5480c2acff4ff1744492b97f4c5089c3d3
rxliuli <rxliuli@gmail.com>
2019-05-23 23:19:42 +0800

    更新函数 [timing], 修改返回值类型

--- src/module/function/timing.ts
+++ src/module/function/timing.ts
@@ -1,5 +1,7 @@
 import { ReturnFunc } from '../interface/ReturnFunc'
 
+type MayPromise<T, R = T> = T extends Promise<any> ? Promise<R> : R
+
 /**
  * 测试函数的执行时间
  * 注：如果函数返回 Promise，则该函数也会返回 Promise，否则直接返回执行时间
@@ -16,3 +18,16 @@ export function timing<R>(fn: ReturnFunc<R>): number | Promise<number> {
   }
   return result.then(() => performance.now() - begin)
 }
+
+// 第二种实现方式，避免了重载，却需要在函数内部进行强制类型转换
+// export function timing<R>(
+//   fn: ReturnFunc<R>,
+//   // 函数返回类型是 Promise 的话，则返回 Promise<number>，否则返回 number
+// ): R extends Promise<any> ? Promise<number> : number {
+//   const begin = performance.now()
+//   const result = fn()
+//   if (!(result instanceof Promise)) {
+//     return convert(performance.now() - begin)
+//   }
+//   return result.then(() => performance.now() - begin) as any
+// }

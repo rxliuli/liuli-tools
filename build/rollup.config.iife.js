a381c996036f8250249795fe752b4b819c45cfb1
rxliuli <rxliuli@gmail.com>
2019-06-01 16:31:25 +0800

    添加 iife 模式的构建脚本, 以使 IDEA 的提示更加准确

---
+++ build/rollup.config.iife.js
@@ -0,0 +1,14 @@
+import { calcPath } from './util'
+import { name } from '../package.json'
+import base from './rollup.config.base'
+
+export default {
+  ...base,
+  output: {
+    ...base.output,
+    // 打包的文件
+    file: calcPath(`../dist/${name}-iife.js`),
+    // 打包的格式，umd 支持 commonjs/amd/life 三种方式
+    format: 'iife',
+  },
+}
--- build/rollup.config.js
+++ build/rollup.config.js
@@ -1,5 +1,6 @@
+import iife from './rollup.config.iife'
 import dev from './rollup.config.dev'
 import es from './rollup.config.es'
 import prod from './rollup.config.prod'
 
-export default [es, dev, prod]
+export default [iife, es, dev, prod]

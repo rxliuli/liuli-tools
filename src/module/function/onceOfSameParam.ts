586f44bb63d7db94f37575f8e9ddbea9bd1d3161
rxliuli <rxliuli@gmail.com>
2019-06-06 18:43:54 +0800

    修改函数 [onceOfSameParam], 改变参数 identity 为具体的值而非函数

--- src/module/function/onceOfSameParam.ts
+++ src/module/function/onceOfSameParam.ts
@@ -4,9 +4,8 @@ import { compatibleAsync } from './compatibleAsync'
 /**
  * 包装一个函数为指定参数只执行一次的函数
  * @param fn 需要包装的函数
- * @param paramConverter 参数转换的函数，参数为需要包装函数的参数
+ * @param identity 参数转换的函数，参数为需要包装函数的参数
  * @returns 需要被包装的函数
- * TODO 高阶函数需要更完善的类型信息，主要是声明接受函数与返回函数的参数/返回值完全一致
  */
 export function onceOfSameParam<R>(
   fn: ReturnFunc<R>,

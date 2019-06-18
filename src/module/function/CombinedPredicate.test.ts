d3de80df9c35fcc5c00ea7098f0d3272df948886
rxliuli <rxliuli@gmail.com>
2019-06-18 22:53:21 +0800

    更新测试 [CombinedPredicate.test], 使用解构简化测试

--- src/module/function/CombinedPredicate.test.ts
+++ src/module/function/CombinedPredicate.test.ts
@@ -4,42 +4,37 @@ import { async } from './async'
 
 describe('test CombinedPredicate', () => {
   const { and, or, not } = CombinedPredicate
+  const { isEmpty, isInteger } = stringValidator
   it('test not', () => {
-    const fn = not(stringValidator.isEmpty)
+    const fn = not(isEmpty)
     expect(fn('1')).toBeTrue()
     expect(fn('')).toBeFalse()
   })
   it('test and', () => {
-    const fn = and(not(stringValidator.isEmpty), stringValidator.isInteger)
+    const fn = and(not(isEmpty), isInteger)
     expect(fn('1')).toBeTrue()
     expect(fn('')).toBeFalse()
     expect(fn('abc')).toBeFalse()
   })
   it('test or', () => {
-    const fn = or(stringValidator.isEmpty, stringValidator.isInteger)
+    const fn = or(isEmpty, isInteger)
     expect(fn('1')).toBeTrue()
     expect(fn('')).toBeTrue()
     expect(fn('abc')).toBeFalse()
   })
   it('test async not', async () => {
-    const fn = not(async(stringValidator.isEmpty))
+    const fn = not(async(isEmpty))
     expect(await fn('a')).toBeTrue()
     expect(await fn('')).toBeFalse()
   })
   it('test async and', async () => {
-    const fn = and(
-      not(async(stringValidator.isEmpty)),
-      async(stringValidator.isInteger),
-    )
+    const fn = and(not(async(isEmpty)), async(isInteger))
     expect(await fn('1')).toBeTrue()
     expect(await fn('')).toBeFalse()
     expect(await fn('abc')).toBeFalse()
   })
   it('test async or', async () => {
-    const fn = or(
-      async(stringValidator.isEmpty),
-      async(stringValidator.isInteger),
-    )
+    const fn = or(async(isEmpty), async(isInteger))
     expect(await fn('1')).toBeTrue()
     expect(await fn('')).toBeTrue()
     expect(await fn('abc')).toBeFalse()

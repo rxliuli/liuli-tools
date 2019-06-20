f03d2b8132dde10d2d420cad7566f780fa0e828a
rxliuli <rxliuli@gmail.com>
2019-06-20 16:53:34 +0800

    更新 class [Logger], 添加 [console] 上全部的函数

--- src/module/hack/logger.ts
+++ src/module/hack/logger.ts
@@ -1,29 +1,10 @@
 import { emptyFunc } from '../function/emptyFunc'
-import { antiDebug } from './AntiDebug'
-import { returnItself } from '../function/returnItself'
 
 /**
  * 自定义的日志类
- * 与浏览器默认的 {@see console} 拥有着完全相同的函数列表，唯一一点区别是包含了一个全局开关用于控制是否输出日志
+ * 主要便于在开发环境下正常显示调试信息，在生产环境则默认关闭它
  */
 export class Logger {
-  /**
-   * 是否开启日志
-   */
-  private _enable!: boolean
-  /**
-   * 重置函数
-   */
-  private reset: Function = returnItself
-  /**
-   * 构造函数
-   * @param options 可选项
-   * @param options.enable 是否开启日志
-   */
-  constructor({ enable = true } = {}) {
-    this.enable = enable
-  }
-
   /**
    * 设置 enable 的 setter 属性，在改变时合并对应的子类对象实现
    * @param enable 是否开启
@@ -38,20 +19,38 @@ export class Logger {
       k => (this[k] = enable ? console[k] : emptyFunc),
     )
   }
+  public debug = console.debug
+  public error = console.error
+  public info = console.info
+  public log = console.log
+  public warn = console.warn
+  public dir = console.dir
+  public dirxml = console.dirxml
+  public table = console.table
+  public trace = console.trace
+  public group = console.group
+  public groupCollapsed = console.groupCollapsed
+  public groupEnd = console.groupEnd
+  public clear = console.clear
+  public count = console.count
+  public assert = console.assert
+  public profile = console.profile
+  public profileEnd = console.profileEnd
+  public time = console.time
+  public timeEnd = console.timeEnd
+  public timeStamp = console.timeStamp
   /**
-   * 获取当前是否开启日志
+   * 是否开启日志
    */
-  public get enable() {
-    return this._enable
+  private _enable: boolean
+  /**
+   * 构造函数
+   * @param options 可选项
+   * @param options.enable 是否开启日志
+   */
+  constructor({ enable = true } = {}) {
+    this._enable = enable
   }
-  /**
-   * 替代原生的 {@link console.log}
-   * 虽然这里只写了一个 log，但事实上 {@link console} 所有的函数都存在
-   * @param message 打印的消息
-   * @param optionalParams 其他参数
-   * @abstract
-   */
-  public log(message: object, ...optionalParams: any[]) {}
 }
 
 /**

90f19de271dddaa0a57c82ee63ca36abcfc18eb8
rxliuli <rxliuli@gmail.com>
2019-06-25 11:57:52 +0800

    更新函数 [deepProxy], 允许使用默认值

--- src/module/obj/deepProxy.ts
+++ src/module/obj/deepProxy.ts
@@ -1,22 +1,26 @@
+import { isNullOrUndefined } from './isNullOrUndefined'
+
 /**
  * 包装对象，使其成为可以任意深度调用而不会出现 undefined 调用的问题
  * 注意: 该函数不能进行递归调用（{@link JSON.stringfy}），一定会造成堆栈溢出的问题（RangeError: Maximum call stack size exceeded）
  * @param obj 任意一个 Object 对象
+ * @param [defaultValue] 默认值，默认为 {}
  * @returns 包装后的对象
  */
-export function deepProxy<T extends object>(obj: T): T | any {
+export function deepProxy<T extends object>(
+  obj: T,
+  defaultValue: any = {},
+): any {
   const handler = {
     get(target: object, k: PropertyKey): object {
-      Reflect.set(
-        target,
-        k,
-        Reflect.has(target, k) ? Reflect.get(target, k) : {},
-      )
-      const v = Reflect.get(target, k)
-      if (typeof v === 'object') {
-        return new Proxy(v, handler)
+      let v = Reflect.get(target, k)
+      if (isNullOrUndefined(v)) {
+        v = defaultValue
       }
-      return v
+      if (typeof v !== 'object') {
+        return v
+      }
+      return new Proxy(v, handler)
     },
   }
   return new Proxy(obj, handler)

11e0065313bddd238b724748d748d7a5c5e982cf
rxliuli <rxliuli@gmail.com>
2019-08-01 13:28:04 +0800

    更新函数 [treeToList], 使用循环替代原先的递归实现并修复了一些错误

--- src/module/tree/treeToList.test.ts
+++ src/module/tree/treeToList.test.ts
@@ -2,6 +2,39 @@ import { treeToList } from './treeToList'
 import { excludeFieldsDeep } from '../obj/excludeFieldsDeep'
 import { nodeBridgeUtil } from './NodeBridgeUtil'
 
+/* #region  错误测试 */
+
+// let a
+// let b
+// a = {
+//   id: 2,
+//   parentId: 1,
+//   child: [{ id: 3, parentId: 2 }, { id: 4, parentId: 2 }],
+// }
+// b = {
+//   id: 5,
+//   parentId: 1,
+//   child: [{ id: 6, parentId: 5 }, { id: 7, parentId: 5 }],
+// }
+// a.child.push(b)
+// b.child.push(a)
+// const tree = {
+//   id: 1,
+//   child: [a, b],
+// }
+
+// console.log(
+//   '结果',
+//   excludeFieldsDeep(
+//     treeToList(tree, {
+//       calcPath: true,
+//     }),
+//     'child',
+//   ),
+// )
+
+/* #endregion */
+
 /**
  * @test {treeToList}
  */
@@ -32,7 +65,9 @@ describe('test treeToList', () => {
       { id: 7, parentId: 5 },
     ]
 
-    expect(excludeFieldsDeep(treeToList(tree), 'child')).toEqual(list)
+    expect(excludeFieldsDeep(treeToList(tree), 'child')).toIncludeAllMembers(
+      list,
+    )
   })
   it('calc path', () => {
     const tree = {
@@ -61,7 +96,7 @@ describe('test treeToList', () => {
     ]
     expect(
       excludeFieldsDeep(treeToList(tree, { calcPath: true }), 'child'),
-    ).toEqual(list)
+    ).toIncludeAllMembers(list)
   })
   it('custom field for node', () => {
     const tree = {
@@ -100,6 +135,45 @@ describe('test treeToList', () => {
         }),
         'childrens',
       ),
-    ).toEqual(list)
+    ).toIncludeAllMembers(list)
+  })
+  it('trampoline reference', () => {
+    let a
+    let b
+    a = {
+      id: 2,
+      parentId: 1,
+      // @ts-ignore
+      child: [{ id: 3, parentId: 2 }, { id: 4, parentId: 2 }],
+    }
+    b = {
+      id: 5,
+      parentId: 1,
+      // @ts-ignore
+      child: [{ id: 6, parentId: 5 }, { id: 7, parentId: 5 }],
+    }
+    a.child.push(b)
+    b.child.push(a)
+    const tree = {
+      id: 1,
+      child: [a, b],
+    }
+    const list = [
+      { id: 1, path: '1' },
+      { id: 2, parentId: 1, path: '1,2' },
+      { id: 3, parentId: 2, path: '1,2,3' },
+      { id: 4, parentId: 2, path: '1,2,4' },
+      { id: 5, parentId: 1, path: '1,5' },
+      { id: 6, parentId: 5, path: '1,5,6' },
+      { id: 7, parentId: 5, path: '1,5,7' },
+    ]
+    expect(
+      excludeFieldsDeep(
+        treeToList(tree, {
+          calcPath: true,
+        }),
+        'child',
+      ),
+    ).toIncludeAllMembers(list)
   })
 })
--- src/module/tree/treeToList.ts
+++ src/module/tree/treeToList.ts
@@ -1,6 +1,10 @@
 import { returnItself } from '../function/returnItself'
 import { treeMapping } from './treeMapping'
 import { INode } from './INode'
+import { ArrayValidator } from '../array/ArrayValidator'
+import { isNullOrUndefined } from '../obj/isNullOrUndefined'
+import { excludeFieldsDeep } from '../obj/excludeFieldsDeep'
+import { nodeBridgeUtil } from './NodeBridgeUtil'
 
 /**
  * 树转列表可选项参数接口
@@ -18,6 +22,47 @@ interface ITreeToListOptoins<T> {
 
 /**
  * 将树节点转为树节点列表
+ * 存在错误
+ * @param root 树节点
+ * @param options 其他选项
+ * @returns 树节点列表
+ */
+export function treeToList2<T>(
+  root: T,
+  {
+    calcPath = false,
+    bridge = returnItself,
+  }: Partial<ITreeToListOptoins<T>> = {},
+): INode[] {
+  const res: INode[] = []
+  const filterSet = new Set<T>()
+  let i = 0
+  // @ts-ignore
+  treeMapping(root, {
+    before(_node, parentPath) {
+      // @ts-ignore
+      console.log(++i, _node)
+      const node = bridge!(_node)
+      if (filterSet.has(_node)) {
+        return node
+      }
+      filterSet.add(_node)
+      // 是否计算全路径
+      if (calcPath) {
+        node.path = (parentPath ? parentPath + ',' : '') + node.id
+      }
+      // 此时追加到数组中
+      res.push(node)
+      return node
+    },
+    paramFn: node => (calcPath ? [node.path] : []),
+  })
+  return res
+}
+
+/**
+ * 将树节点转为树节点列表
+ * 这里使用了循环进行遍历，而非传统的递归方式
  * @param root 树节点
  * @param options 其他选项
  * @returns 树节点列表
@@ -30,19 +75,48 @@ export function treeToList<T>(
   }: Partial<ITreeToListOptoins<T>> = {},
 ): INode[] {
   const res: INode[] = []
-  // @ts-ignore
-  treeMapping(root, {
-    before(_node, parentPath) {
-      const node = bridge!(_node)
-      // 是否计算全路径
-      if (calcPath) {
-        node.path = (parentPath ? parentPath + ',' : '') + node.id
-      }
-      // 此时追加到数组中
-      res.push(node)
-      return node
-    },
-    paramFn: node => (calcPath ? [node.path] : []),
-  })
+  const temp = bridge(root)
+  if (calcPath) {
+    temp.path = temp.id + ''
+  }
+  // 利用队列缓存所有未处理的节点
+  const queue: INode[] = [temp]
+  // 使用 Set 防止可能的重复引用
+  const filterSet = new Set<INode>()
+  // 使用 lastIdMap 避免重复添加
+  const lastIdMap = new Map()
+  for (let value: INode; queue.length > 0; ) {
+    const first = queue.shift() as any
+    value = bridge(first)
+    // 判断重复
+    if (value === undefined || filterSet.has(first)) {
+      continue
+    }
+    filterSet.add(first)
+    res.push(value)
+    const child = value.child
+    if (ArrayValidator.isEmpty(child)) {
+      continue
+    }
+    const childNonIllegal = child.filter(
+      v => !isNullOrUndefined(v) || filterSet.has(v),
+    )
+    // TODO 这里和上面的代码明显重复，待优化。。。
+    queue.push(
+      ...(calcPath
+        ? childNonIllegal.map(v => {
+            const _v = bridge(v as any)
+            // 如果最后一个的 id 等于自身，说明已经被添加过了
+            if (lastIdMap.get(_v.id) === _v.id) {
+              return _v
+            }
+            // 使用父节点绝对路径 + 当前 id
+            _v.path = value.path + ',' + _v.id
+            lastIdMap.set(_v.id, _v.id)
+            return _v
+          })
+        : childNonIllegal),
+    )
+  }
   return res
 }

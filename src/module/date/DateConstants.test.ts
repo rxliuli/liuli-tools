7bf58f9ea6c72c8e895002ee96cb223d39a74289
rxliuli <rxliuli@gmail.com>
2019-09-20 11:03:29 +0800

    更新 class [DateConstants]，添加函数 [monthStart, monthEnd]

--- src/module/date/DateConstants.test.ts
+++ src/module/date/DateConstants.test.ts
@@ -1,31 +1,39 @@
-import { dateConstants } from './DateConstants'
+import { DateConstants } from './DateConstants'
 
 /**
- * @test {dateConstants}
+ * @test {DateConstants}
  */
-describe('test dateConstants', () => {
+describe('test DateConstants', () => {
   it('test get day start', () => {
-    const date = dateConstants.dayStart()
+    const date = DateConstants.dayStart()
     expect(date.getHours()).toBe(0)
     expect(date.getMinutes()).toBe(0)
     expect(date.getSeconds()).toBe(0)
     expect(date.getMilliseconds()).toBe(0)
   })
   it('test get day end', () => {
-    const date = dateConstants.dayEnd()
+    const date = DateConstants.dayEnd()
     expect(date.getHours()).toBe(23)
     expect(date.getMinutes()).toBe(59)
     expect(date.getSeconds()).toBe(59)
     expect(date.getMilliseconds()).toBe(999)
   })
+  it('test get month start', () => {
+    const date = DateConstants.monthStart()
+    expect(date.getDate()).toBe(1)
+  })
+  it('test get month end', () => {
+    const date = DateConstants.monthEnd(new Date('2000-02-11'))
+    expect(date.getDate()).toBe(29)
+  })
   it('test get year start', () => {
-    const date = dateConstants.yearStart()
+    const date = DateConstants.yearStart()
     expect(date.getFullYear()).toBe(new Date().getFullYear())
     expect(date.getMonth()).toBe(0)
     expect(date.getDate()).toBe(1)
   })
   it('test get year end', () => {
-    const date = dateConstants.yearEnd()
+    const date = DateConstants.yearEnd()
     expect(date.getFullYear()).toBe(new Date().getFullYear())
     expect(date.getMonth()).toBe(11)
     expect(date.getDate()).toBe(31)
--- src/module/date/DateConstants.ts
+++ src/module/date/DateConstants.ts
@@ -1,6 +1,23 @@
 import { dateFormat } from './dateFormat'
 
 /**
+ * 计算月有多少天
+ * @param date 日期
+ * @returns 月的总天数
+ */
+function calcMonEndDay(date: Date) {
+  const monthToDay: Array<[Set<number>, number]> = [
+    [new Set([1, 3, 5, 7, 8, 10, 12]), 30],
+    [new Set([4, 6, 9, 11]), 30],
+    [new Set([2]), 28],
+  ]
+  const year = date.getFullYear()
+  const month = date.getMonth() + 1
+  const days = monthToDay.find(([monthSet]) => monthSet.has(month))![1]
+  return days + (month === 2 && year % 4 === 0 ? 1 : 0)
+}
+
+/**
  * 日期固定时间点
  */
 export class DateConstants {
@@ -19,6 +36,24 @@ export class DateConstants {
    */
   public static dayEnd(date: Date = new Date()): Date {
     return new Date(`${dateFormat(date, 'yyyy-MM-dd')}T23:59:59.999`)
+  }
+  /**
+   * 获取指定日期所在月的开始时间
+   * @param date 指定的时间，默认为当前日期
+   * @returns 月的开始时间
+   */
+  public static monthStart(date: Date = new Date()): Date {
+    return new Date(`${dateFormat(date, 'yyyy-MM')}-01T00:00:00.000`)
+  }
+  /**
+   * 获取指定日期所在月的结束时间
+   * @param date 指定的时间，默认为当前日期
+   * @returns 月的结束时间
+   */
+  public static monthEnd(date: Date = new Date()): Date {
+    return new Date(
+      `${dateFormat(date, 'yyyy-MM')}-${calcMonEndDay(date)}T23:59:59.999`,
+    )
   }
   /**
    * 获取指定日期所在年份的新年开始时间

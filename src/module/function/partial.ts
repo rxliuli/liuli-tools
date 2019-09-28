540a6653e1cc8750c1150c27e55b061440e4b8f9
rxliuli <rxliuli@gmail.com>
2019-09-28 14:26:11 +0800

    添加函数 [partial], 以替代命名存在问题的 curry

--- src/module/function/partial.ts
+++ src/module/function/partial.ts
@@ -1,11 +1,11 @@
 /**
- * 柯里化后返回的函数
+ * 部分应用后返回的函数
  */
 interface IRFunc extends Function {
   /**
-   * 是否为柯里化函数，默认为 true 以标识柯里化函数
+   * 是否为偏函数，默认为 true 以标识偏函数
    */
-  _curry: boolean
+  _partial: boolean
   /**
    * 剩余参数的长度
    */
@@ -17,15 +17,14 @@ interface IRFunc extends Function {
 }
 
 /**
- * 将函数包装为柯里化函数
- * 注: 该函数模仿了 Lodash 的 curry 函数
+ * 将函数包装为偏函数
+ * 注: 该函数模仿了 underscore 的 partial 函数
  * @param fn 需要包装的函数
  * @param  {...any} args 应用的部分参数
  * @returns 包装后的函数
- * @deprecated 由于之前的理解错误，该函数在下个大版本将会被废弃，请使用命名更合适的 {@link partial}
  */
-export function curry(fn: Function, ...args: any[]): IRFunc {
-  const realArgs = args.filter(arg => arg !== curry._)
+export function partial(fn: Function, ...args: any[]): IRFunc {
+  const realArgs = args.filter(arg => arg !== partial._)
   // 如果函数参数足够则调用传入的函数
   if (realArgs.length >= fn.length) {
     return fn(...realArgs)
@@ -42,31 +41,31 @@ export function curry(fn: Function, ...args: any[]): IRFunc {
     let i = 0
     const newArgs = args.map(arg => {
       if (
-        arg !== curry._ ||
+        arg !== partial._ ||
         otherArgs[i] === undefined ||
-        otherArgs[i] === curry._
+        otherArgs[i] === partial._
       ) {
         return arg
       }
       removeIndexSet.add(i)
-      // 每次补偿前面的 curry._ 参数计数器 +1
+      // 每次补偿前面的 partial._ 参数计数器 +1
       return otherArgs[i++]
     })
     const newOtherArgs = otherArgs.filter((_v, i) => !removeIndexSet.has(i))
-    return curry(fn, ...newArgs, ...newOtherArgs)
+    return partial(fn, ...newArgs, ...newOtherArgs)
   }
-  // 定义柯里化函数的剩余参数长度，便于在其他地方进行部分参数应用
+  // 定义偏函数的剩余参数长度，便于在其他地方进行部分参数应用
   // 注: 不使用 length 属性的原因是 length 属性
-  innerFn._length = fn.length - args.filter(arg => arg !== curry._).length
+  innerFn._length = fn.length - args.filter(arg => arg !== partial._).length
   // 自定义 toString 函数便于调试
   innerFn.toString = () =>
     `name: ${fn.name}, args: [${args.map(o => o.toString()).join(', ')}]`
-  innerFn._curry = true
+  innerFn._partial = true
   return innerFn
 }
 
 /**
- * 柯里化的占位符，需要应用后面的参数时使用
- * 例如 {@link curry(fn)(curry._, 1)} 意味着函数 fn 的第二个参数将被确定为 1
+ * 偏的占位符，需要应用后面的参数时使用
+ * 例如 {@link partial(fn)(partial._, 1)} 意味着函数 fn 的第二个参数将被确定为 1
  */
-curry._ = Symbol('_')
+partial._ = Symbol('_')

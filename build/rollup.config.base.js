70d5fb5530a1cd8bd4542a29025e1038d2ae10a4
rxliuli <rxliuli@gmail.com>
2019-10-21 12:19:02 +0800

    删除 tslint，之后改用 eslint for typescript

--- build/rollup.config.base.js
+++ build/rollup.config.base.js
@@ -1,5 +1,4 @@
 import typescript from 'rollup-plugin-typescript2'
-import tslint from 'rollup-plugin-tslint'
 import { calcPath } from './util'
 
 export default {
@@ -12,8 +11,6 @@ export default {
     sourcemap: true,
   },
   plugins: [
-    // 引入 tslint 插件，必须在 babel 之前引入，因为 babel 编译之后的代码未必符合 tslint 规范，tslint 仅针对我们 [原本] 的代码
-    tslint(),
     typescript({
       exclude: ['./dist', './src/**/*.test.ts'],
     }),

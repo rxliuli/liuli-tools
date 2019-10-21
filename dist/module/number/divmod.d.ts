87b32f3ccac03e01c05f0d929121bec858921a18
rxliuli <rxliuli@gmail.com>
2019-10-21 12:29:35 +0800

    添加函数 [divmod], 灵感来自于 Python 中的 divmod，暂时不会添加到 rx 上  https://www.programiz.com/python-programming/methods/built-in/divmod

---
+++ dist/module/number/divmod.d.ts
@@ -0,0 +1,8 @@
+/**
+ * 同时计算除数和余数
+ * @param a 被除数
+ * @param b 除数
+ * @returns 结果和余数的元组（数组）
+ */
+export declare function divmod(a: number, b: number): [number, number];
+//# sourceMappingURL=divmod.d.ts.map

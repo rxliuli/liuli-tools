fbc4f7ce1eb0e79629582c30f31acc7c939488f6
rxliuli <rxliuli@gmail.com>
2019-10-21 12:42:33 +0800

    添加函数 [insert], 可以在数组指定位置插入元素

---
+++ src/module/array/insert.test.ts
@@ -0,0 +1,19 @@
+import { insert } from './insert'
+
+describe('测试 insert', function() {
+  it('简单示例', () => {
+    const arr = [1, 2]
+    //测试插入一条数据
+    insert(arr, 0, 3)
+    expect(arr).toIncludeAllMembers([3, 1, 2])
+    //插入多条
+    insert(arr, 0, 4, 5)
+    expect(arr).toIncludeAllMembers([4, 5, 3, 1, 2])
+    //测试返回的位置
+    expect(insert(arr, 1, 1)).toBe(1)
+    let len = arr.length
+    expect(insert(arr, -1, 1)).toBe(len - 1)
+    len = arr.length
+    expect(insert(arr, 100, 1)).toBe(len)
+  })
+})

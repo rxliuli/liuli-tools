eb9c6d49f9731bedb6678bc3d73a0792c0da2964
rxliuli <rxliuli@gmail.com>
2020-03-19 19:59:39 +0800

    更新 class [ArrayUtil], 修改 [swap] 函数（不兼容），添加函数 [equals]（未完成）

--- src/module/array/ArrayUtil.ts
+++ src/module/array/ArrayUtil.ts
@@ -1,3 +1,5 @@
+import { sortBy } from './sortBy'
+
 /**
  * 数组相关工具类
  */
@@ -7,8 +9,21 @@ export class ArrayUtil {
    * @param arr 数组
    * @param i 第一个下标
    * @param k 第二个下标
+   * @returns 交换后的数组
    */
-  static swap(arr: any[], i: number, k: number): void {
-    ;[arr[i], arr[k]] = [arr[k], arr[i]]
+  static swap<T>(arr: T[], i: number, k: number): T[] {
+    const res = arr.slice()
+    ;[res[i], res[k]] = [res[k], res[i]]
+    return res
+  }
+
+  /**
+   * 无序比较两个数组
+   * @param arr1 第一个数组
+   * @param arr2 第二个数组
+   * @returns 是否相同
+   */
+  static equalsByDisorder<T>(arr1: T[], arr2: T[]): boolean {
+    return JSON.stringify(sortBy(arr1)) === JSON.stringify(sortBy(arr2))
   }
 }

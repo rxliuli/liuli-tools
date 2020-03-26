bf829553b8746f0475777ff13bb6a5eb2b47c180
rxliuli <rxliuli@gmail.com>
2020-03-27 00:13:49 +0800

    添加函数 [stream], 用于演示延迟数组操作是怎么实现的

---
+++ src/module/idea/stream.ts
@@ -0,0 +1,49 @@
+enum ActionEnum {
+  Filter,
+  Map,
+}
+
+interface Task {
+  type: ActionEnum
+  action: Function
+}
+
+class Stream<T> {
+  private taskList: Task[] = []
+  constructor(private arr: T[]) {}
+
+  filter(fn: (val: T) => boolean) {
+    this.taskList.push({
+      type: ActionEnum.Filter,
+      action: fn,
+    })
+    return this
+  }
+  map<U>(fn: (val: T) => U) {
+    this.taskList.push({
+      type: ActionEnum.Map,
+      action: fn,
+    })
+    const stream = new Stream<U>(this.arr as any)
+    stream.taskList = this.taskList
+    return stream
+  }
+  value() {
+    return this.arr.reduce((res, v) => {
+      let temp = v
+      for (const task of this.taskList) {
+        if (task.type === ActionEnum.Filter && !task.action(temp)) {
+          return res
+        } else if (task.type === ActionEnum.Map) {
+          temp = task.action(temp)
+        }
+      }
+      res.push(temp)
+      return res
+    }, [] as T[])
+  }
+}
+
+export function stream<T>(arr: T[]) {
+  return new Stream(arr)
+}

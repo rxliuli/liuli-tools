6ac04fde91c32532d836f51debdb3cf8f38b8578
rxliuli <rxliuli@gmail.com>
2020-03-27 23:33:56 +0800

    更新函数 [pipe], 优化类型定义

--- src/module/idea/pipe.test.ts
+++ src/module/idea/pipe.test.ts
@@ -1,26 +1,37 @@
 import { pipe } from './pipe'
 
 describe('测试 pipe', () => {
+  it('使用普通的高阶函数', () => {
+    const res = pipe(
+      Array(10),
+      arr => arr.fill(0) as number[],
+      arr => arr.map((_, i) => i),
+      arr => arr.filter(i => i % 2 === 0),
+      arr => arr.map(i => i.toString()),
+      arr => arr.reduce((res, s) => res.concat(s.split('')), [] as string[]),
+      arr => arr.join(','),
+    )
+    expect(res).toBe('0,2,4,6,8')
+  })
   it('基本示例', () => {
-    type DescArray<T extends any[]> = T extends (infer U)[] ? U : never
-    const fill = <T, A extends T[]>(value: T, start?: number, end?: number) => (
-      arr: A,
-    ) => arr.fill(value, start, end)
-    const filter = <A extends any[]>(
-      func: (value: DescArray<A>, index: number, array: A) => boolean,
-    ) => (arr: A) => arr.filter(func as any)
-    const map = <A extends any[], U>(
-      func: (value: DescArray<A>, index: number, array: A) => U,
-    ) => (arr: A) => arr.map(func as any)
-    const join = <T, A extends T[]>(separator?: string) => (arr: A) =>
-      arr.join(separator)
+    const fill = <T>(val: T) => (arr: any[]) => arr.fill(val) as T[]
+    const filter = <T>(func: (val: T) => boolean) => (arr: T[]) =>
+      arr.filter(func)
+    const map = <T, U>(func: (val: T, i: number) => U) => (arr: T[]) =>
+      arr.map(func)
+    const join = <T>(separator?: string) => (arr: T[]) => arr.join(separator)
+    const reduce = <T, R>(func: (res: R, val: T) => R, init: R) => (arr: T[]) =>
+      arr.reduce(func, init)
 
     const res = pipe(
+      Array(10),
       fill(0),
       map((_, i) => i),
-      filter((i: any) => i % 2 === 1),
+      filter(i => i % 2 === 0),
+      map(i => i.toString()),
+      reduce((res, s) => res.concat(s.split('')), [] as string[]),
       join(','),
-    )(Array(10))
-    expect(res).toEqual('1,3,5,7,9')
+    )
+    expect(res).toEqual('0,2,4,6,8')
   })
 })

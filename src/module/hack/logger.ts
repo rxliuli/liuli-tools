c8719033825b37fb85417733a442b862ddf775cd
rxliuli <rxliuli@gmail.com>
2020-04-15 16:10:53 +0800

    更新函数 [Logger], 添加 level（日志级别）设置

--- src/module/hack/logger.ts
+++ src/module/hack/logger.ts
@@ -1,55 +1,89 @@
 import { emptyFunc } from '../function/emptyFunc'
 
+enum LoggerLevelEnum {
+  Debug = 0,
+  Log,
+  Info,
+  Warn,
+  Error,
+}
+
+const enumMap: Partial<Record<keyof typeof console, LoggerLevelEnum>> = {
+  debug: LoggerLevelEnum.Debug,
+  log: LoggerLevelEnum.Log,
+  info: LoggerLevelEnum.Info,
+  warn: LoggerLevelEnum.Warn,
+  error: LoggerLevelEnum.Error,
+}
+
 /**
  * 自定义的日志类
  * 主要便于在开发环境下正常显示调试信息，在生产环境则默认关闭它
  */
 export class Logger {
+  static Level = LoggerLevelEnum
   /**
    * 设置 enable 的 setter 属性，在改变时合并对应的子类对象实现
    * @param enable 是否开启
    */
-  public set enable(enable: boolean) {
+  set enable(enable: boolean) {
     /**
      * @field 是否开启全局控制台，该属性只写
      */
-    this._enable = enable
-    Object.keys(console).forEach(
-      // @ts-ignore
-      k => (this[k] = enable ? console[k] : emptyFunc),
+    ;(Object.keys(console) as (keyof typeof console)[]).forEach(k =>
+      Reflect.set(this, k, enable ? console[k] : emptyFunc),
     )
   }
-  public debug = console.debug
-  public error = console.error
-  public info = console.info
-  public log = console.log
-  public warn = console.warn
-  public dir = console.dir
-  public dirxml = console.dirxml
-  public table = console.table
-  public trace = console.trace
-  public group = console.group
-  public groupCollapsed = console.groupCollapsed
-  public groupEnd = console.groupEnd
-  public clear = console.clear
-  public count = console.count
-  public assert = console.assert
-  public profile = console.profile
-  public profileEnd = console.profileEnd
-  public time = console.time
-  public timeEnd = console.timeEnd
-  public timeStamp = console.timeStamp
+
   /**
-   * 是否开启日志
+   * 设置日志的级别
+   * @param level
    */
-  private _enable: boolean
+  set level(level: LoggerLevelEnum) {
+    ;(Object.keys(console) as (keyof typeof console)[])
+      .filter(k => Reflect.has(enumMap, k))
+      .forEach(k =>
+        Reflect.set(
+          this,
+          k,
+          Reflect.get(enumMap, k) >= level ? console[k] : emptyFunc,
+        ),
+      )
+  }
+  debug = console.debug
+  error = console.error
+  info = console.info
+  log = console.log
+  warn = console.warn
+  dir = console.dir
+  dirxml = console.dirxml
+  table = console.table
+  trace = console.trace
+  group = console.group
+  groupCollapsed = console.groupCollapsed
+  groupEnd = console.groupEnd
+  clear = console.clear
+  count = console.count
+  assert = console.assert
+  profile = console.profile
+  profileEnd = console.profileEnd
+  time = console.time
+  timeEnd = console.timeEnd
+  timeStamp = console.timeStamp
+
   /**
    * 构造函数
    * @param options 可选项
    * @param options.enable 是否开启日志
    */
-  constructor({ enable = true } = {}) {
-    this._enable = enable
+  constructor(
+    {
+      enable = true,
+      level = LoggerLevelEnum.Log,
+    }: Pick<Logger, 'enable' | 'level'> = {} as any,
+  ) {
+    this.enable = enable
+    this.level = level
   }
 }
 

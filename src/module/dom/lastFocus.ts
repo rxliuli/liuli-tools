c5890c9e1d8aca9189c0a9a6e8e9f503a25101d1
rxliuli <rxliuli@gmail.com>
2020-06-09 15:13:01 +0800

    更新函数 [lastFocus], 默认不再添加 `focus/blur` 事件

--- src/module/dom/lastFocus.ts
+++ src/module/dom/lastFocus.ts
@@ -2,24 +2,29 @@ import { Nullable } from '../interface/Nullable'
 
 let lastFocusEl: Nullable<Element> | EventTarget
 
-document.addEventListener(
-  'focus',
-  event => {
-    lastFocusEl = event.target
-  },
-  true,
-)
-document.addEventListener(
-  'blur',
-  () => {
-    lastFocusEl = null
-  },
-  true,
-)
 /**
  * 获取到最后一个获得焦点的元素
  * @returns 最后一个获取到焦点的元素
  */
-export function lastFocus(): Nullable<Element> | EventTarget {
+function _lastFocus(): Nullable<Element> | EventTarget {
   return lastFocusEl
 }
+
+export const lastFocus = Object.assign(_lastFocus, {
+  init() {
+    document.addEventListener(
+      'focus',
+      event => {
+        lastFocusEl = event.target
+      },
+      true,
+    )
+    document.addEventListener(
+      'blur',
+      () => {
+        lastFocusEl = null
+      },
+      true,
+    )
+  },
+})

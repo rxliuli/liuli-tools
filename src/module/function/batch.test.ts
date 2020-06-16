ab7aa6b18ab1e20aa970c324414b6d1ffe18a1a7
rxliuli <rxliuli@gmail.com>
2020-06-17 00:23:28 +0800

    更新函数 [batch], 支持异步批处理

--- src/module/function/batch.test.ts
+++ src/module/function/batch.test.ts
@@ -1,8 +1,25 @@
 import { batch } from './batch'
+import { arrayToMap } from '../array/arrayToMap'
 
 describe('测试 batch', () => {
-  it('基本示例', () => {
-    const fn = async (i: number) => i * 2
-    console.log(fn)
+  it('基本示例', async () => {
+    const handle = jest.fn(async (idList: [number][]) => {
+      console.log('batch.handle', idList)
+      return arrayToMap(
+        idList,
+        params => params,
+        ([id]) => id * 2,
+      )
+    })
+    const fn = batch(async (i: number) => i * 2, handle)
+    const numbers = await Promise.all([fn(1), fn(2)])
+    expect(numbers).toEqual([2, 4])
+    //上面虽然调用了两次，但批处理只会进行一次，因为它们是并发调用的，所以只会处理一次
+    expect(handle.mock.calls.length).toBe(1)
+    // console.log('numbers: ', numbers)
+    expect(await fn(3)).toBe(6)
+    expect(await fn(4)).toBe(8)
+    //如果是顺序调用则仍然会执行两次批处理
+    expect(handle.mock.calls.length).toBe(3)
   })
 })
--- src/module/function/batch.ts
+++ src/module/function/batch.ts
@@ -1,24 +1,35 @@
 import { wait } from '../async/wait'
 import { Func, DeconstructionPromise } from 'liuli-types'
+import { Err } from 'typedoc/dist/lib/utils/result'
 
 export function batch<
   T extends Func,
   P extends Parameters<T>,
   R extends DeconstructionPromise<ReturnType<T>>
->(fn: T, handle: (args: Set<P>) => Map<P, R>) {
-  const map = new Map<P, R | Error>()
-  const pList = new Set<P>()
+>(fn: T, handle: (args: P[]) => Promise<Map<P, R>>) {
+  const resultMap = new Map<P, R | Error>()
+  const paramSet = new Set<P>()
   let lock = false
   return new Proxy(fn, {
     async apply(target: T, thisArg: any, argArray: P): Promise<R> {
-      pList.add(argArray)
-      await wait(() => map.get(argArray) !== undefined || !lock)
+      paramSet.add(argArray)
+      // console.log('apply wait begin: ', lock, resultMap.get(argArray))
+      await wait(() => resultMap.get(argArray) !== undefined || !lock)
+      // console.log('apply wait end: ', lock, resultMap.get(argArray), resultMap)
       lock = true
       try {
-        Array.from((await handle(pList)).entries()).forEach(([k, v]) => {
-          map.set(k, v)
-        })
-        const value = map.get(argArray)
+        if (!resultMap.has(argArray)) {
+          Array.from((await handle(Array.from(paramSet))).entries()).forEach(
+            ([k, v]) => {
+              resultMap.set(k, v)
+            },
+          )
+        }
+        const value = resultMap.get(argArray)
+        paramSet.delete(argArray)
+        resultMap.delete(argArray)
+        // console.log('delete: ', resultMap)
+
         if (value instanceof Error) {
           throw value
         }

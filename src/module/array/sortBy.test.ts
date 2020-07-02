7ab27957bded23bfa3c784affa41f46b70ca3e83
rxliuli <rxliuli@gmail.com>
2020-07-02 09:41:34 +0800

    更新 function [sort], 添加一个错误的测试用例

--- src/module/array/sortBy.test.ts
+++ src/module/array/sortBy.test.ts
@@ -4,13 +4,13 @@ import { CombinedPredicate } from '../function/CombinedPredicate'
 /**
  * @test {sortBy}
  */
-describe('test sortBy', () => {
-  it('simple example', () => {
+describe('测试 sortBy', () => {
+  it('基本示例', () => {
     const arr = [5, 1, 2, 4, 3]
     expect(sortBy(arr)).toIncludeSameMembers([1, 2, 3, 4, 5])
     expect(sortBy(arr, i => -i)).toIncludeSameMembers([5, 4, 3, 2, 1])
   })
-  it('test object item array', () => {
+  it('测试对象数组', () => {
     class User {
       public name: any
       public age: any
@@ -36,5 +36,14 @@ describe('test sortBy', () => {
       new User('琉璃', 18),
       new User('rx', 17),
     ])
+  })
+  it.skip('测试根据外部的值进行获取', () => {
+    const map = {
+      1: 2,
+      2: 1,
+      3: 1,
+    }
+    const arr: (keyof typeof map)[] = [1, 2, 3]
+    expect(sortBy(arr, k => map[k])).toEqual([2, 3, 1])
   })
 })

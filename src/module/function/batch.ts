3e04b92e52be3dd43968b9b4f0cb5f7d3f8b2ca1
rxliuli <rxliuli@gmail.com>
2020-07-03 19:25:37 +0800

    更新函数 [batch], 修复缓存问题（未完成）

--- src/module/function/batch.ts
+++ src/module/function/batch.ts
@@ -1,16 +1,27 @@
 import { wait } from '../async/wait'
+import {
+  BaseMemoryCache,
+  MemoryCacheEnum,
+  MemoryCacheFactory,
+} from '../cache/MemoryCache'
 
 /**
  * 将多个并发异步调用合并为一次批处理
  * @param handle 批处理的函数
  * @param ms 等待的时长（时间越长则可能合并的调用越多，否则将使用微任务只合并一次同步执行的所有调用）
+ * @param cache 缓存对象，必须限制大小避免数据过期
  */
 export function batch<P extends any[], R extends any>(
   handle: (list: P[]) => Promise<Map<P, R | Error>>,
   ms: number = 0,
+  cache: BaseMemoryCache<string, R | Error> = MemoryCacheFactory.create(
+    MemoryCacheEnum.Lru,
+    {
+      limit: 100,
+    },
+  ),
 ): (...args: P) => Promise<R> {
   //参数 => 结果 映射
-  const resultMap = new Map<string, R | Error>()
   //参数列表
   const paramSet = new Set<string>()
   //当前是否被锁定
@@ -18,22 +29,21 @@ export function batch<P extends any[], R extends any>(
   return async function(...args: P) {
     const argArray = JSON.stringify(args)
     paramSet.add(argArray)
-    await Promise.all([wait(() => resultMap.has(argArray) || !lock), wait(ms)])
-    if (!resultMap.has(argArray)) {
+    await Promise.all([wait(() => cache.has(argArray) || !lock), wait(ms)])
+    if (!cache.has(argArray)) {
       try {
         lock = true
         Array.from(
           await handle(Array.from(paramSet).map(v => JSON.parse(v))),
         ).forEach(([k, v]) => {
-          resultMap.set(JSON.stringify(k), v)
+          cache.add(JSON.stringify(k), v)
         })
       } finally {
         lock = false
       }
     }
-    const value = resultMap.get(argArray)
+    const value = cache.get(argArray)
     paramSet.delete(argArray)
-    resultMap.delete(argArray)
 
     // noinspection SuspiciousTypeOfGuard
     if (value instanceof Error) {

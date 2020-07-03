20bc113604b60c5699dce7db9ad48ab816ccc6b4
rxliuli <rxliuli@gmail.com>
2020-07-03 16:09:48 +0800

    更新函数 [batch], 修复对象类型默认不会深度比较的问题

--- src/module/function/batch.ts
+++ src/module/function/batch.ts
@@ -10,19 +10,22 @@ export function batch<P extends any[], R extends any>(
   ms: number = 0,
 ): (...args: P) => Promise<R> {
   //参数 => 结果 映射
-  const resultMap = new Map<P, R | Error>()
+  const resultMap = new Map<string, R | Error>()
   //参数列表
-  const paramSet = new Set<P>()
+  const paramSet = new Set<string>()
   //当前是否被锁定
   let lock = false
-  return async function(...argArray: P) {
+  return async function(...args: P) {
+    const argArray = JSON.stringify(args)
     paramSet.add(argArray)
     await Promise.all([wait(() => resultMap.has(argArray) || !lock), wait(ms)])
     if (!resultMap.has(argArray)) {
       try {
         lock = true
-        Array.from(await handle(Array.from(paramSet))).forEach(([k, v]) => {
-          resultMap.set(k, v)
+        Array.from(
+          await handle(Array.from(paramSet).map(v => JSON.parse(v))),
+        ).forEach(([k, v]) => {
+          resultMap.set(JSON.stringify(k), v)
         })
       } finally {
         lock = false

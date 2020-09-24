5caf6841a51f673840d5f94b1f5b4b7bd2d72a43
rxliuli <rxliuli@gmail.com>
2020-09-24 12:26:43 +0800

    提交 v1.0.12 遗漏的内容

---
+++ dist/module/ajax/splitHttpHeader.d.ts
@@ -0,0 +1,7 @@
+/**
+ * 分割 http 请求中 header 的内容为一个 map
+ * 分隔符参考 {@link https://developer.mozilla.org/zh-CN/docs/Web/HTTP/Headers/Content-Type}
+ * @param header
+ */
+export declare function splitHttpHeader(header: string): Map<any, any>;
+//# sourceMappingURL=splitHttpHeader.d.ts.map

45e6c749345590eb9ce87c8151391d6aa14b9e97
rxliuli <rxliuli@gmail.com>
2021-01-13 17:47:10 +0800

    增强 groupBy 函数，支持自定义 reduce 阶段的操作

--- libs/array/src/__tests__/groupBy.test.ts
+++ libs/array/src/__tests__/groupBy.test.ts
@@ -7,4 +7,18 @@ describe('测试 groupBy', () => {
       new Map().set(true, [2]).set(false, [1, 3]),
     )
   })
+  it('测试自定义 reduce 函数', () => {
+    const arr = [
+      { type: 1, sub: [1] },
+      { type: 2, sub: [2] },
+      { type: 1, sub: [3] },
+    ]
+    expect(
+      groupBy(
+        arr,
+        (item) => item.type,
+        (res, item) => res.concat(item.sub),
+      ),
+    ).toEqual(new Map().set(1, [1, 3]).set(2, [2]))
+  })
 })
--- libs/array/src/groupBy.ts
+++ libs/array/src/groupBy.ts
@@ -7,13 +7,31 @@
 export function groupBy<T, K>(
   arr: T[],
   kFn: (item: T, index: number, arr: T[]) => K,
-): Map<K, T[]> {
+): Map<K, T[]>
+/**
+ * 将数组按照指定条件分组
+ * @param arr 要进行分组的数组
+ * @param kFn 元素分组的唯一标识函数，会将返回 key 作为键，将相同 key 的元素归类为一个数组作为值
+ * @param vFn 元素分组的值处理的函数。第一个参数是累计值，第二个参数是当前正在迭代的元素，如果你使用过 {@link Array#reduce} 函数的话应该对此很熟悉
+ * @returns 元素标识 => 数组映射 Map
+ */
+export function groupBy<T, K, R>(
+  arr: T[],
+  kFn: (item: T, index: number, arr: T[]) => K,
+  vFn: (res: R[], item: T, index: number, arr: T[]) => R[],
+): Map<K, T[]>
+export function groupBy<T, K, R>(
+  arr: T[],
+  kFn: (item: T, index: number, arr: T[]) => K,
+  vFn: (res: R[], item: T, index: number, arr: T[]) => R[] = (res, item) => {
+    res.push(item as any)
+    return res
+  },
+): Map<K, R[]> {
   // 将元素按照分组条件进行分组得到一个 条件 -> 数组 的对象
   return arr.reduce((res, item, index, arr) => {
     const k = kFn(item, index, arr)
-    const curr = res.get(k) || []
-    curr.push(item)
-    res.set(k, curr)
+    res.set(k, vFn(res.get(k) || [], item, index, arr))
     return res
-  }, new Map<K, T[]>())
+  }, new Map<K, R[]>())
 }

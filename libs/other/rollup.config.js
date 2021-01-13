85e078096d50f5981eddbe4662f5cc3c0ce9948c
rxliuli <rxliuli@gmail.com>
2021-01-13 16:06:09 +0800

    为所有模块添加 cjs 支持

--- libs/other/rollup.config.js
+++ libs/other/rollup.config.js
@@ -5,11 +5,18 @@ import { terser } from 'rollup-plugin-terser'
 export default {
   // 入口文件
   input: './src/index.ts',
-  output: {
-    // 打包名称
-    file: './dist/index.js',
-    format: 'esm',
-  },
+  output: [
+    {
+      // 打包名称
+      file: './dist/index.js',
+      format: 'cjs',
+    },
+    {
+      // 打包名称
+      file: './dist/index.esm.js',
+      format: 'esm',
+    },
+  ],
   external: [...Object.keys(pkg.dependencies || {})],
   plugins: [
     typescript({

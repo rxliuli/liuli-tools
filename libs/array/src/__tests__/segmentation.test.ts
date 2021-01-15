ef3222e518cd3e4b1a5655936966c9667686132d
rxliuli <rxliuli@gmail.com>
2021-01-15 15:00:36 +0800

    内部使用 groupBy 重构函数 segmentation，修复它在在处理数量为 1 的数组时出现的错误

--- libs/array/src/__tests__/segmentation.test.ts
+++ libs/array/src/__tests__/segmentation.test.ts
@@ -1,14 +1,19 @@
 import { segmentation } from '../segmentation'
 
-describe('test segmentation', () => {
-  it('simple example', () => {
-    expect(segmentation([1, 2, 3, 4, 5], 2)).toEqual([[1, 2], [3, 4], [5]])
-    expect(segmentation([1, 2, 3, 4, 5], 3)).toEqual([
+describe('测试 segmentation', () => {
+  const arr = [1, 2, 3, 4, 5]
+  it('基本示例', () => {
+    expect(segmentation(arr, 2)).toEqual([[1, 2], [3, 4], [5]])
+    expect(segmentation(arr, 3)).toEqual([
       [1, 2, 3],
       [4, 5],
     ])
   })
-  it('exception', () => {
-    expect(() => segmentation([1, 2, 3, 4, 5], 1)).toThrow()
+  it('测试数组为空', () => {
+    expect(segmentation([], 1)).toEqual([])
+  })
+
+  it('测试按照 1 进行分割', () => {
+    expect(segmentation(arr, 1).flat()).toEqual(arr)
   })
 })
--- libs/array/src/segmentation.ts
+++ libs/array/src/segmentation.ts
@@ -1,3 +1,5 @@
+import { groupBy } from './groupBy'
+
 /**
  * 数组按照指定长度进行分段为二维数组
  * 注: num 必须要大于 1
@@ -5,13 +7,7 @@
  * @param num 每段的长度
  * @returns 分段后的二维数组
  */
+
 export function segmentation<T>(arr: T[], num: number): T[][] {
-  return arr.reduce((res, v, i) => {
-    const index = (i + 1) % num
-    if (index === 1) {
-      res.push([])
-    }
-    res[res.length - 1].push(v)
-    return res
-  }, new Array<T[]>())
+  return Array.from(groupBy(arr, (item, i) => Math.floor(i / num)).values())
 }

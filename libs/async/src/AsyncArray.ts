dbb50e4dd46a268d30093c09d96467f959c84e88
rxliuli <rxliuli@gmail.com>
2021-01-26 18:44:27 +0800

    更新 AsyncArray，删除串行和并行的区别

--- libs/async/src/AsyncArray.ts
+++ libs/async/src/AsyncArray.ts
@@ -1,139 +1,10 @@
 import { ValuesType } from 'utility-types'
-import { ClassUtil } from '@liuli-util/object'
-
-/**
- * 异步数组的基本接口
- * 仅声明一些纯函数，真正的链式异步调用在外层 API 实现
- */
-export interface BaseAsyncArray {
-  reduce<T extends any[], R>(
-    arr: T,
-    fn: (res: R, item: ValuesType<T>, index: number) => Promise<R>,
-    res: R,
-  ): Promise<R>
-
-  map<T extends any[], R>(
-    arr: T,
-    fn: (item: ValuesType<T>, index: number) => Promise<R>,
-  ): Promise<R[]>
-
-  filter<T extends any[]>(
-    arr: T,
-    fn: (item: ValuesType<T>, index: number) => Promise<boolean>,
-  ): Promise<T[]>
-
-  forEach<T extends any[]>(
-    arr: T,
-    fn: (item: ValuesType<T>, index: number) => Promise<void>,
-  ): Promise<void>
-}
-
-export abstract class BasicAsyncArray
-  implements Pick<BaseAsyncArray, 'reduce'> {
-  reduce<T extends any[], R>(
-    arr: T,
-    fn: (res: R, item: ValuesType<T>, index: number) => Promise<R>,
-    res: R,
-  ): Promise<R> {
-    return arr.reduce(
-      (res: Promise<R>, item: ValuesType<T>, index: number) =>
-        res.then((r) => fn(r, item, index)),
-      Promise.resolve(res),
-    )
-  }
-}
-
-export class SerialAsyncArray
-  extends BasicAsyncArray
-  implements BaseAsyncArray {
-  map<T, R>(
-    arr: T[],
-    fn: (item: T, index: number) => Promise<R>,
-  ): Promise<R[]> {
-    return this.reduce(
-      arr,
-      async (res, ...args) => {
-        res.push(await fn(...args))
-        return res
-      },
-      [] as R[],
-    )
-  }
-
-  async filter<T>(
-    arr: T[],
-    fn: (item: T, index: number) => Promise<boolean>,
-  ): Promise<T[]> {
-    return await this.reduce(
-      arr,
-      async (res, item, index) => {
-        if (await fn(item, index)) {
-          res.push(item)
-        }
-        return res
-      },
-      [] as T[],
-    )
-  }
-
-  async forEach<T extends any[]>(
-    arr: T,
-    fn: (item: ValuesType<T>, index: number) => Promise<void>,
-  ): Promise<void> {
-    await this.reduce(
-      arr,
-      async (res, item, index) => {
-        await fn(item, index)
-        return res
-      },
-      null,
-    )
-  }
-}
-
-export class ParallelAsyncArray
-  extends BasicAsyncArray
-  implements BaseAsyncArray {
-  map<T, R>(
-    arr: T[],
-    fn: (item: T, index: number) => Promise<R>,
-  ): Promise<R[]> {
-    return Promise.all(arr.map((item, index) => fn(item, index)))
-  }
-
-  async filter<T>(
-    arr: T[],
-    fn: (item: T, index: number) => Promise<boolean>,
-  ): Promise<T[]> {
-    const res: T[] = []
-    await this.map(arr, async (item, index) => {
-      if (await fn(item, index)) {
-        res.push(item)
-      }
-    })
-    return res
-  }
-
-  async forEach<T extends any[]>(
-    arr: T,
-    fn: (item: ValuesType<T>, index: number) => Promise<void>,
-  ): Promise<void> {
-    await this.map(arr, fn)
-  }
-}
-
-export const serialAsyncArray = ClassUtil.bindMethodThis(new SerialAsyncArray())
-export const parallelAsyncArray = ClassUtil.bindMethodThis(
-  new ParallelAsyncArray(),
-)
 
 enum ActionTypeEnum {
   Filter = 'filter',
   Map = 'map',
   ForEach = 'forEach',
   Reduce = 'reduce',
-  Parallel = 'parallel',
-  Serial = 'serial',
 }
 
 class Action {
@@ -148,7 +19,49 @@ class Action {
   }
 }
 
+/**
+ * 异步数组，支持静态方法和链式调用
+ */
 export class AsyncArray<T> implements PromiseLike<T[]> {
+  static reduce<T extends any[], R>(
+    arr: T,
+    fn: (res: R, item: ValuesType<T>, index: number) => Promise<R>,
+    res: R,
+  ): Promise<R> {
+    return arr.reduce(
+      (res: Promise<R>, item: ValuesType<T>, index: number) =>
+        res.then((r) => fn(r, item, index)),
+      Promise.resolve(res),
+    )
+  }
+
+  static map<T, R>(
+    arr: T[],
+    fn: (item: T, index: number) => Promise<R>,
+  ): Promise<R[]> {
+    return Promise.all(arr.map((item, index) => fn(item, index)))
+  }
+
+  static async filter<T>(
+    arr: T[],
+    fn: (item: T, index: number) => Promise<boolean>,
+  ): Promise<T[]> {
+    const res: T[] = []
+    await AsyncArray.map(arr, async (item, index) => {
+      if (await fn(item, index)) {
+        res.push(item)
+      }
+    })
+    return res
+  }
+
+  static async forEach<T extends any[]>(
+    arr: T,
+    fn: (item: ValuesType<T>, index: number) => Promise<void>,
+  ): Promise<void> {
+    await AsyncArray.map(arr, fn)
+  }
+
   private tasks: Action[] = []
 
   constructor(private readonly arr: T[]) {}
@@ -160,16 +73,6 @@ export class AsyncArray<T> implements PromiseLike<T[]> {
 
   filter(fn: (item: T, index: number) => Promise<boolean>): this {
     this.tasks.push(new Action(ActionTypeEnum.Filter, [fn]))
-    return this
-  }
-
-  parallel(): this {
-    this.tasks.push(new Action(ActionTypeEnum.Parallel, []))
-    return this
-  }
-
-  serial(): this {
-    this.tasks.push(new Action(ActionTypeEnum.Serial, []))
     return this
   }
 
@@ -197,32 +100,20 @@ export class AsyncArray<T> implements PromiseLike<T[]> {
   }
 
   private async value(): Promise<any> {
-    let isSerial = false
-
-    function getAsyncArray() {
-      return isSerial ? serialAsyncArray : parallelAsyncArray
-    }
-
     let res = this.arr
     for (const task of this.tasks) {
       switch (task.type) {
         case ActionTypeEnum.Filter:
-          res = await getAsyncArray().filter(res, task.args[0])
+          res = await AsyncArray.filter(res, task.args[0])
           break
         case ActionTypeEnum.Map:
-          res = await getAsyncArray().map(res, task.args[0])
+          res = await AsyncArray.map(res, task.args[0])
           break
         case ActionTypeEnum.ForEach:
-          await getAsyncArray().forEach(res, task.args[0])
+          await AsyncArray.forEach(res, task.args[0])
           return
         case ActionTypeEnum.Reduce:
-          return await getAsyncArray().reduce(res, task.args[0], task.args[1])
-        case ActionTypeEnum.Parallel:
-          isSerial = false
-          break
-        case ActionTypeEnum.Serial:
-          isSerial = true
-          break
+          return await AsyncArray.reduce(res, task.args[0], task.args[1])
       }
     }
     return res
--- libs/async/src/__tests__/AsyncArray.test.ts
+++ libs/async/src/__tests__/AsyncArray.test.ts
@@ -1,44 +1,36 @@
-import {
-  AsyncArray,
-  BaseAsyncArray,
-  parallelAsyncArray,
-  serialAsyncArray,
-} from '../AsyncArray'
+import { AsyncArray } from '../AsyncArray'
 import { wait } from '../wait'
 import { countTime, Expect } from '@liuli-util/test'
 
 describe('测试 AsyncArray', () => {
-  async function testReduce(reduce: BaseAsyncArray['reduce']) {
-    const res = await reduce([1, 2, 3, 4], async (res, item) => res + item, 0)
+  async function testReduce(reduce: Function) {
+    const res = await reduce(
+      [1, 2, 3, 4],
+      async (res: any, item: number) => res + item,
+      0,
+    )
     expect(res).toBe(10)
   }
 
-  async function testMap(map: BaseAsyncArray['map']) {
-    const res = await map([1, 2, 3, 4, 5], async (item) => item * 2)
+  async function testMap(map: Function) {
+    const res = await map([1, 2, 3, 4, 5], async (item: number) => item * 2)
     expect(res).toEqual([2, 4, 6, 8, 10])
   }
 
-  async function testFilter(filter: BaseAsyncArray['filter']) {
-    const res = await filter([1, 2, 3, 4, 5], async (item) => item % 2 === 0)
+  async function testFilter(filter: Function) {
+    const res = await filter(
+      [1, 2, 3, 4, 5],
+      async (item: number) => item % 2 === 0,
+    )
     expect(res).toEqual([2, 4])
   }
 
-  async function testForEach(forEach: BaseAsyncArray['forEach']) {
+  async function testForEach(forEach: Function) {
     const fn = jest.fn(async (item: number) => {
       console.log(item)
     })
     await forEach([1, 2, 3, 4, 5], fn)
     expect(fn.mock.calls.length).toBe(5)
-  }
-
-  async function testSerialTime(
-    fn: (arr: any[], fn: (...args: any[]) => Promise<any>) => Promise<any>,
-  ) {
-    const num = 5
-    const time = 100
-    expect(
-      await countTime(() => fn(Array(num).fill(0), () => wait(time))),
-    ).toBeGreaterThanOrEqual(num * time)
   }
 
   async function testParallelTime(
@@ -51,40 +43,21 @@ describe('测试 AsyncArray', () => {
     ).toBeLessThanOrEqual(num * time)
   }
 
-  describe('测试 SerialAsyncArray', () => {
-    const arr = serialAsyncArray
+  describe('测试 AsyncArray', () => {
     it('测试 reduce', async () => {
-      await testReduce(arr.reduce)
+      await testReduce(AsyncArray.reduce)
     })
     it('测试 map', async () => {
-      await testMap(arr.map)
-      await testSerialTime(arr.map)
+      await testMap(AsyncArray.map)
+      await testParallelTime(AsyncArray.map)
     })
     it('测试 filter', async () => {
-      await testFilter(arr.filter)
-      await testSerialTime(arr.filter)
+      await testFilter(AsyncArray.filter)
+      await testParallelTime(AsyncArray.filter)
     })
     it('测试 forEach', async () => {
-      await testForEach(arr.forEach)
-      await testSerialTime(arr.forEach)
-    })
-  })
-  describe('测试 ParallelAsyncArray', () => {
-    const arr = parallelAsyncArray
-    it('测试 reduce', async () => {
-      await testReduce(arr.reduce)
-    })
-    it('测试 map', async () => {
-      await testMap(arr.map)
-      await testParallelTime(arr.map)
-    })
-    it('测试 filter', async () => {
-      await testFilter(arr.filter)
-      await testParallelTime(arr.filter)
-    })
-    it('测试 forEach', async () => {
-      await testForEach(arr.forEach)
-      await testParallelTime(arr.forEach)
+      await testForEach(AsyncArray.forEach)
+      await testParallelTime(AsyncArray.forEach)
     })
   })
   describe('测试 AsyncArray', () => {
@@ -103,9 +76,7 @@ describe('测试 AsyncArray', () => {
         return i % 2 === 0
       })
       const res = await new AsyncArray(Array(10).fill(0))
-        .parallel()
         .map(async (_, i) => i)
-        .serial()
         .filter(fn)
       expect(res.every((i) => i % 2 === 0)).toBe(true)
     })

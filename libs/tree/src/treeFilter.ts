acf429839aa3fc8bf0f26b8b57787cd1d25db613
rxliuli <rxliuli@gmail.com>
2021-02-01 12:50:47 +0800

    将 treeFilter 筑基于 treeMap 上

--- libs/tree/src/treeFilter.ts
+++ libs/tree/src/treeFilter.ts
@@ -1,4 +1,5 @@
 import { TreeOption } from './treeOption'
+import { treeMap } from './treeMap'
 
 /**
  * 过滤一个树节点列表
@@ -11,29 +12,29 @@ export function treeFilter<T extends object, C extends TreeOption<T>>(
   fn: (t: T, path: T[C['id']][]) => boolean,
   options: C,
 ): T[] {
-  function inner(nodeList: T[], parentPath: T[C['id']][]): T[] {
-    return nodeList
-      .map((node) => {
-        const children = (node[options.children] as any) as T[] | undefined
-        //不筛选叶子节点
-        if (!children) {
-          return node
-        }
-        return {
-          ...node,
-          [options.children]: inner(children, [
-            ...parentPath,
-            node[options.id],
-          ]),
-        }
-      })
-      .filter((node) => {
-        const path = [...parentPath, node[options.id]]
-        const children = (node[options.children] as any) as T[] | undefined
-        //筛选掉空的枝干节点
-        return fn(node, path) && (children === undefined || children.length > 0)
-      })
-  }
-
-  return inner(nodeList, [])
+  return treeMap(
+    nodeList,
+    (node: any, path) => {
+      const children = (node[options.children] as any) as T[] | undefined
+      //如果是错误的节点直接炸掉
+      if (!fn(node as T, path)) {
+        return null
+      }
+      //如果是叶子节点就返回
+      if (!children) {
+        return node
+      }
+      //计算所有子节点中不是 null 的子节点
+      const sub = children.filter((node) => node !== null)
+      //如果所有子节点为 null 就炸掉
+      if (sub.length === 0) {
+        return null
+      }
+      return {
+        ...node,
+        children: sub,
+      }
+    },
+    options,
+  ).filter((node) => node !== null)
 }

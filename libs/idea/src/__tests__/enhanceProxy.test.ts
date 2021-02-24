136c6ba14dbc0a74a16acb6a432cd934c8e515be
rxliuli <rxliuli@gmail.com>
2021-02-24 10:56:00 +0800

    添加实验用的 idea 模块

---
+++ libs/idea/src/__tests__/enhanceProxy.test.ts
@@ -0,0 +1,26 @@
+import { enhanceProxy, EnhanceProxyHandler } from '../enhanceProxy'
+import * as console from 'console'
+
+describe('测试 enhanceProxy', () => {
+  it('基本示例', () => {
+    interface U {
+      info: {
+        address: {
+          city: string
+        }
+      }
+    }
+
+    const handler: EnhanceProxyHandler<object> = {
+      get(target: any, path: PropertyKey[]): any {
+        if (path.join('.') === 'info.address.city') {
+          return '广州'
+        }
+        return enhanceProxy({}, handler)
+      },
+    }
+    const res = enhanceProxy<U>({} as any, handler)
+    expect(res.info.address.city).toBe('广州')
+    expect(() => console.log(res.info)).toThrowError()
+  })
+})
---
+++ libs/idea/src/enhanceProxy.ts
@@ -0,0 +1,52 @@
+import { Overwrite } from 'utility-types'
+
+export type EnhanceProxyHandler<T extends object> = Overwrite<
+  ProxyHandler<T>,
+  {
+    get?(target: T, path: PropertyKey[], receiver: any): any
+    set?(target: T, path: PropertyKey[], value: any, receiver: any): boolean
+    apply?(target: T, path: PropertyKey[], thisArg: any, argArray?: any): any
+  }
+>
+
+const key = Symbol('key')
+
+/**
+ * 增强 proxy
+ * 添加参数 path 表示属性访问路径
+ * @param obj
+ * @param handler
+ */
+export function enhanceProxy<T extends object>(
+  obj: T,
+  handler: EnhanceProxyHandler<T>,
+) {
+  const _handler: ProxyHandler<T> = {}
+  if (handler.get) {
+    _handler.get = (target: T, p: PropertyKey, receiver: any): any => {
+      if (p === 'toString') {
+        throw new Error()
+      }
+      const path = (((target as any)[key] as any[]) || []).concat(p)
+      const res = handler.get!(target, path, receiver)
+      if (typeof res === 'object') {
+        Reflect.set(res, key, path)
+      }
+      return res
+    }
+  }
+  if (handler.set) {
+    _handler.set = (
+      target: T,
+      p: PropertyKey,
+      value: any,
+      receiver: any,
+    ): boolean => {
+      return false
+    }
+  }
+  if (handler.apply) {
+    _handler.apply = (target: T, thisArg: any, argArray?: any): any => {}
+  }
+  return new Proxy(obj, _handler)
+}

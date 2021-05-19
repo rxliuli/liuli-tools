ff8996f59388612b12d37f57d1aa622b8fbfc081
rxliuli <rxliuli@gmail.com>
2021-05-19 18:13:05 +0800

    feat(@liuli-util/cli): 删除根据配置打包的功能

--- apps/liuli-cli/src/commands/build/index.ts
+++ apps/liuli-cli/src/commands/build/index.ts
@@ -1,27 +1,29 @@
 import { Command } from 'commander'
 import { BuildProgram } from './BuildProgram'
-import { TypeScriptConfigFile } from '../../utils'
-import path from 'path'
 
 const buildProgram = new BuildProgram()
 export const buildCommand = new Command('build')
   .description('使用 rollup 将 ts lib 打包到 dist 目录，格式为 esm/cjs')
-  .option('-c --config [config]', '使用自定义配置文件')
   .action(async (option: { watch?: boolean; config?: true | string }) => {
-    if (!!option.config) {
-      const configFile = new TypeScriptConfigFile({
-        configFile: option.config === true ? 'rollup.config.ts' : option.config,
-        configRoot: path.resolve('.'),
-      })
-      const options = await configFile.load()
-      await buildProgram.build(
-        Array.isArray(options) ? options : [options],
-        !!option.watch,
-      )
-    } else {
-      await buildProgram.buildPkg(!!option.watch)
-    }
+    await buildProgram.buildPkg(!!option.watch)
   })
+  // .addCommand(
+  //   new Command('config')
+  //     .description('根据 rollup.config.ts 配置打包')
+  //     .option('-w --watch', '监视模式')
+  //     .option('-c --config [config]', '使用自定义配置文件')
+  //     .action(async (option: { watch?: boolean; config?: true | string }) => {
+  //       const configFile = new TypeScriptConfigFile({
+  //         configFile: typeof option.config === 'string' ? option.config : 'rollup.config.ts',
+  //         configRoot: path.resolve('.'),
+  //       })
+  //       const options = await configFile.load()
+  //       await buildProgram.build(
+  //         Array.isArray(options) ? options : [options],
+  //         !!option.watch,
+  //       )
+  //     }),
+  // )
   .addCommand(
     new Command('pkg')
       .description('使用 rollup 将 ts lib 打包到 dist 目录，格式为 esm/cjs')

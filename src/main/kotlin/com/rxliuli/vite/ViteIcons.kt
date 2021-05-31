d3dee7f3249117edf02baea5489e77dde04743fa
rxliuli <rxliuli@gmail.com>
2021-05-31 18:46:51 +0800

    fix: 修复使用过时的 API IconLoader.getIcon 的问题

--- src/main/kotlin/com/rxliuli/vite/ViteIcons.kt
+++ src/main/kotlin/com/rxliuli/vite/ViteIcons.kt
@@ -3,6 +3,6 @@ package com.rxliuli.vite
 import com.intellij.openapi.util.IconLoader
 
 object ViteIcons {
-    val ICON_16 = IconLoader.getIcon("/icons/icon16.png")
-    val ICON_24 = IconLoader.getIcon("/icons/icon24.png")
+    val ICON_16 = IconLoader.getIcon("/icons/icon16.png", ViteIcons.javaClass)
+    val ICON_24 = IconLoader.getIcon("/icons/icon24.png", ViteIcons.javaClass)
 }

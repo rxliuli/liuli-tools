ad448a3dc64b504baa64abf3b4efbde2d4b60d7b
rxliuli <rxliuli@gmail.com>
2021-06-03 20:41:59 +0800

    feat(jetbrains-plugins): 将 jetbrains-plugins 目录作为 monorepo 使用 gradle 单独管理

---
+++ jetbrains-plugins/build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    kotlin("jvm") version "1.5.10"
+}
+
+group = "org.rxliuli"
+version = "0.1.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
---
+++ jetbrains-plugins/settings.gradle.kts
@@ -0,0 +1,3 @@
+rootProject.name = "jetbrains-plugins"
+include("vite-jetbrains-plugin")
+

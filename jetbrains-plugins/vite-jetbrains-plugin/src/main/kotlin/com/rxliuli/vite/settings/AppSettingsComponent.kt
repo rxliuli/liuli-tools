ddeffab5a64b679dee4a24848f7e52fa7f91cf5f
rxliuli <rxliuli@gmail.com>
2021-06-08 13:00:09 +0800

    refactor(vite-jetbrains-plugin): 使用更加精简的 kotlin ui dsl getter/setter 代码

--- jetbrains-plugins/vite-jetbrains-plugin/src/main/kotlin/com/rxliuli/vite/settings/AppSettingsComponent.kt
+++ jetbrains-plugins/vite-jetbrains-plugin/src/main/kotlin/com/rxliuli/vite/settings/AppSettingsComponent.kt
@@ -2,22 +2,16 @@ package com.rxliuli.vite.settings
 
 import com.intellij.openapi.ui.DialogPanel
 import com.intellij.ui.layout.panel
-import javax.swing.JPanel
 
 class AppSettingsComponent {
     private val settings: AppSettingsState = AppSettingsState.instance
+
     val panel: DialogPanel = panel {
         row("Enter user name: ") {
-            textField({
-                settings.userId
-            }, setter = { v ->
-                settings.userId = v
-            }).focused()
+            textField({ settings.userId }, { settings.userId = it }).focused()
         }
         row {
-            checkBox("Do you use IntelliJ IDEA? ", { settings.ideaStatus }, { v ->
-                settings.ideaStatus = v
-            })
+            checkBox("Do you use IntelliJ IDEA? ", { settings.ideaStatus }, { settings.ideaStatus = it })
         }
     }
 }

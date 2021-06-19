5b5aa52dc10756559b2c9124db0130b9959ec76f
rxliuli <rxliuli@gmail.com>
2021-06-19 21:52:58 +0800

    fix(@liuli-util/shell): 修复 commander 要求可变参数必须是最后一个带来的一些问题

--- apps/shell/src/lib.ts
+++ apps/shell/src/lib.ts
@@ -1,30 +1,27 @@
 import { Command } from 'commander'
 import { ShellProgram } from './index'
 
-export const removeCmd = new Command()
-  .command('rm <...files>')
-  .action(async (_1, _2, cmd) => {
+export const removeCmd = new Command('rm')
+  .action(async (_options, cmd) => {
     await ShellProgram.remove(cmd.args)
   })
   .description('删除文件或目录')
 
-export const copyCmd = new Command()
-  .command('cp <...sources> <dest>')
-  .action(async (_1, _2, cmd) => {
+export const copyCmd = new Command('cp')
+  .action(async (_options, cmd) => {
     await ShellProgram.copy(cmd.args)
   })
   .description('复制文件或目录')
 
-export const moveCmd = new Command()
-  .command('mv <...sources> <dest>')
-  .action(async (_1, _2, cmd) => {
+export const moveCmd = new Command('mv')
+  .action(async (_options, cmd) => {
     await ShellProgram.move(cmd.args)
   })
   .description('移动文件或目录')
 
 export const mkdirCmd = new Command()
-  .command('mkdir <...dirs>')
-  .action(async (_1, _2, cmd) => {
+  .command('mkdir')
+  .action(async (_options, cmd) => {
     await ShellProgram.mkdir(cmd.args)
   })
   .description('递归创建目录')

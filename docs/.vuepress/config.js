72bd2ea491dc46bea01e3aec55cfcedf5e2f6184
rxliuli <rxliuli@gmail.com>
2021-06-21 06:03:07 +0800

    docs: 添加国际化支持

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,15 +1,45 @@
 module.exports = {
+  locales: {
+    '/': {
+      lang: 'en-US', // 将会被设置为 <html> 的 lang 属性
+      title: 'liuli-tools',
+      description:
+        'Some project-level toolsets mainly try to develop and improve efficiency. Welcome to [Discussion](https://github.com/rxliuli/liuli-tools/discussions) to exchange ideas.',
+    },
+    '/zh/': {
+      lang: 'zh-CN',
+      title: 'liuli-tools',
+      description:
+        '一些项目级别的工具集，主要尝试研发提效，欢迎来 [讨论区](https://github.com/rxliuli/liuli-tools/discussions) 交流想法。',
+    },
+  },
   themeConfig: {
-    nav: [
-      { text: '首页', link: '/' },
-      { text: '指南', link: '/guide/' },
-      {
-        text: 'GitHub',
-        link: 'https://github.com/rxliuli/liuli-tools',
+    locales: {
+      '/': {
+        nav: [
+          { text: 'Home', link: '/' },
+          {
+            text: 'GitHub',
+            link: 'https://github.com/rxliuli/liuli-tools',
+          },
+        ],
+        sidebar: {
+          '/guide/': [''],
+        },
       },
-    ],
-    sidebar: {
-      '/guide/': [''],
+      '/zh/': {
+        nav: [
+          { text: '首页', link: '/zh/' },
+          { text: '指南', link: '/zh/guide/' },
+          {
+            text: 'GitHub',
+            link: 'https://github.com/rxliuli/liuli-tools',
+          },
+        ],
+        sidebar: {
+          '/zh/guide/': [''],
+        },
+      },
     },
   },
 }
--- docs/README.md
+++ docs/README.md
@@ -1,15 +1,13 @@
 ---
 home: true
 heroText: Web Project Tools
-tagline: 一些项目级别的工具集，主要尝试研发提效，欢迎来 GitHub 讨论区交流想法。
-actionLink: /guide/
-actionText: 快速上手 →
+tagline: A collection of project-level tools, mainly to try to improve efficiency in R&D. Feel free to come to the GitHub discussion forum to exchange ideas.
 features:
   - title: '@liuli-util/cli'
-    details: 一个针对于库和 CLI 应用程序打包的零配置 CLI
+    details: A zero-configuration CLI for packaging libraries and CLI applications
   - title: '@liuli-util/shell'
-    details: 在一个 cli 中包含所有常用的 unix 命令
+    details: all common unix commands in one cli
   - title: jetbrains-plugins
-    details: 一些 jetbrains ide 的插件，从 ide 角度做的尝试
+    details: some plugins for jetbrains ide, an attempt to do it from an ide perspective
 footer: MIT Licensed | Copyright © 2021-present rxliuli
 ---

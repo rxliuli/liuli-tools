06165d3441afb733edbcbc75e09bd2db5a38eba5
rxliuli <rxliuli@gmail.com>
2021-08-29 11:56:24 +0800

    fix: 修复模板中的一些依赖缺失和脚本错误的问题

--- apps/liuli-cli/src/bin.ts
+++ apps/liuli-cli/src/bin.ts
@@ -3,7 +3,7 @@ import { buildCommand } from './commands/build'
 import { initCommand } from './commands/init'
 import { syncCommand } from './commands/sync'
 
-const main = new Command('pinefield')
+const main = new Command()
 main
   .addCommand(buildCommand)
   .addCommand(initCommand)

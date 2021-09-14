61b20c676c04e564970e6f432e7a54ff0a9b9d5f
rxliuli <rxliuli@gmail.com>
2021-09-14 22:49:48 +0800

    chore: 修复一些打包的问题

--- libs/yarn-plugin-changed/src/utils/__tests__/lodash.test.ts
+++ libs/yarn-plugin-changed/src/utils/__tests__/lodash.test.ts
@@ -1,10 +1,10 @@
-import { differenceBy, uniqBy } from 'lodash'
+import { differenceBy, uniqBy } from 'lodash';
 
 describe('测试 diffBy', () => {
   it('基本示例', () => {
-    const res = differenceBy([1, 3], [3, 4])
-    expect(res).toEqual([1])
-  })
+    const res = differenceBy([1, 3], [3, 4]);
+    expect(res).toEqual([1]);
+  });
   it('对象类型', () => {
     const res = differenceBy(
       [
@@ -17,19 +17,21 @@ describe('测试 diffBy', () => {
         [3, 1],
         [4, 1],
       ],
-      (item) => item.join(','),
-    )
-    expect(res.map((item) => item[0])).toEqual([1, 2])
-  })
-})
+      (item) => item.join(',')
+    );
+    expect(res.map((item) => item[0])).toEqual([1, 2]);
+  });
+});
 
 describe('测试 uniqBy', () => {
-  const res = uniqBy(
-    [
-      [1, 1],
-      [2, 1],
-    ],
-    (item) => item[1],
-  )
-  expect(res).toEqual([[1, 1]])
-})
+  it('测试过滤得到结果的顺序', () => {
+    const res = uniqBy(
+      [
+        [1, 1],
+        [2, 1],
+      ],
+      (item) => item[1]
+    );
+    expect(res).toEqual([[1, 1]]);
+  });
+});
--- libs/yarn-plugin-changed/src/utils/listChangedWorkspaces.ts
+++ libs/yarn-plugin-changed/src/utils/listChangedWorkspaces.ts
@@ -1,15 +1,15 @@
-import { Project, Workspace } from '@yarnpkg/core'
-import getWorkspaceDependents from './getWorkspaceDependents'
-import { calcModuleHash } from './calcModuleHash'
-import { differenceBy, uniqBy } from 'lodash'
-import { NativePath, npath } from '@yarnpkg/fslib'
-import { BuildCache, WorkspaceCacheInfo } from './BuildCache'
-import { FilterOptions, filterWorkspaces } from './filterWorkspaces'
+import { Project, Workspace } from '@yarnpkg/core';
+import getWorkspaceDependents from './getWorkspaceDependents';
+import { calcModuleHash } from './calcModuleHash';
+import { differenceBy, uniqBy } from 'lodash';
+import { NativePath, npath } from '@yarnpkg/fslib';
+import { BuildCache, WorkspaceCacheInfo } from './BuildCache';
+import { FilterOptions, filterWorkspaces } from './filterWorkspaces';
 
 export type CacheWorkspace = {
-  updateCache(): Promise<void>
-  workspaces: Workspace[]
-}
+  updateCache(): Promise<void>;
+  workspaces: Workspace[];
+};
 
 /**
  * 获取所有改变的模块
@@ -24,12 +24,12 @@ export async function listChangedWorkspaces({
   include,
   exclude,
 }: {
-  project: Project
-  cmd: string
+  project: Project;
+  cmd: string;
 } & FilterOptions): Promise<CacheWorkspace> {
-  const rootPath = npath.fromPortablePath(project.cwd)
-  const buildCache = new BuildCache(rootPath)
-  const last = await buildCache.get(cmd)
+  const rootPath = npath.fromPortablePath(project.cwd);
+  const buildCache = new BuildCache(rootPath);
+  const last = await buildCache.get(cmd);
   const current = await Promise.all(
     filterWorkspaces({
       workspaces: project.workspaces,
@@ -39,26 +39,24 @@ export async function listChangedWorkspaces({
       return {
         ...(await calcModuleHash(rootPath, npath.fromPortablePath(item.cwd))),
         cwd: npath.fromPortablePath(item.cwd),
-      } as WorkspaceCacheInfo
-    }),
-  )
+      } as WorkspaceCacheInfo;
+    })
+  );
   const addOrUpdateWorkspaceCwdSet = new Set(
     differenceBy(current, last, (item) => JSON.stringify(item)).map(
-      (item) => item.cwd as NativePath,
-    ),
-  )
-
-  const workspaces = new Set<Workspace>()
-  for (const item of project.workspaces) {
-    const changed = addOrUpdateWorkspaceCwdSet.has(
-      npath.fromPortablePath(item.cwd),
+      (item) => item.cwd as NativePath
     )
+  );
+
+  const workspaces = new Set<Workspace>();
+  for (const item of project.workspaces) {
+    const changed = addOrUpdateWorkspaceCwdSet.has(npath.fromPortablePath(item.cwd));
 
     if (changed && !workspaces.has(item)) {
-      workspaces.add(item)
+      workspaces.add(item);
 
       for (const dep of getWorkspaceDependents(item)) {
-        workspaces.add(dep)
+        workspaces.add(dep);
       }
     }
   }
@@ -68,8 +66,8 @@ export async function listChangedWorkspaces({
     async updateCache() {
       await buildCache.set(
         cmd,
-        uniqBy([...current, ...last], (item) => item.cwd),
-      )
+        uniqBy([...current, ...last], (item) => item.cwd)
+      );
     },
-  }
+  };
 }

5983a29190e9a6cac2d5532ac9d5e8c0011e4418
rxliuli <rxliuli@gmail.com>
2021-10-26 00:48:47 +0800

    feat: 使用 flag 避免多次运行监视器

--- libs/rollup-plugin-i18next-dts-gen/src/index.ts
+++ libs/rollup-plugin-i18next-dts-gen/src/index.ts
@@ -5,12 +5,21 @@ import {
 } from '@liuli-util/i18next-dts-gen'
 import { SetOptional } from 'type-fest'
 
+/**
+ * 生成 i18next dts 的插件
+ * @param options
+ */
 export function i18nextDtsGen(
   options: SetOptional<Omit<GenerateOptions, 'watch'>, 'language'>,
 ): Plugin {
+  let watchFlag = false
   return {
     name: 'rollup-plugin-i18next-dts-gen',
     async buildStart() {
+      if (watchFlag) {
+        return
+      }
+      watchFlag = true
       await new GeneratorCommandProgram().main({
         language: 'en',
         ...options,

0f666fdbeee514f614a61b5257c4aa9a6cfd3143
rxliuli <rxliuli@gmail.com>
2021-11-14 20:41:52 +0800

    fix: 修复 generate 不能选择模板的问题

--- apps/liuli-cli/src/commands/generate/GenerateProgram.ts
+++ apps/liuli-cli/src/commands/generate/GenerateProgram.ts
@@ -1,14 +1,5 @@
 import path from 'path'
-import {
-  copy,
-  pathExists,
-  readdir,
-  readFile,
-  readJSON,
-  remove,
-  writeFile,
-  writeJSON,
-} from 'fs-extra'
+import { copy, pathExists, readdir, readFile, readJSON, remove, writeFile, writeJSON } from 'fs-extra'
 import { prompt } from 'enquirer'
 import { SyncProgram } from '../sync/SyncProgram'
 import { PathUtil } from '../../PathUtil'
@@ -43,12 +34,9 @@ export class GenerateProgram {
     if (!config.template) {
       const { template } = await prompt<{ template: TemplateTypeEnum }>({
         name: 'template',
-        type: 'list',
+        type: 'select',
         message: '请选择模板',
-        choices: [
-          TemplateTypeEnum.Lib,
-          TemplateTypeEnum.Cli,
-        ] as TemplateTypeEnum[],
+        choices: [TemplateTypeEnum.Lib, TemplateTypeEnum.Cli] as TemplateTypeEnum[],
       })
       config.template = template
     }
@@ -60,15 +48,9 @@ export class GenerateProgram {
       - 修改 package.json，删除 private，修改名字
     模板特定修改
      */
-    const srcFile = path.resolve(
-      PathUtil.RootPath,
-      `templates/${config.template}`,
-    )
+    const srcFile = path.resolve(PathUtil.RootPath, `templates/${config.template}`)
     const destFile = path.resolve(config.dest)
-    if (
-      (await pathExists(destFile)) &&
-      (await readdir(destFile)).some((file) => pathExists(file))
-    ) {
+    if ((await pathExists(destFile)) && (await readdir(destFile)).some((file) => pathExists(file))) {
       const { override } = await prompt<{
         override: boolean
       }>({

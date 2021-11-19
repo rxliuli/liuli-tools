adafd0e2489dfc8ef3283d262d05e3e33f9cb68d
rxliuli <rxliuli@gmail.com>
2021-11-20 02:21:43 +0800

    fix: 修复过期的 API

--- jetbrains-plugins/vite-jetbrains-plugin/src/main/kotlin/com/rxliuli/vite/settings/AppSettingsState.kt
+++ jetbrains-plugins/vite-jetbrains-plugin/src/main/kotlin/com/rxliuli/vite/settings/AppSettingsState.kt
@@ -1,7 +1,7 @@
 package com.rxliuli.vite.settings
 
+import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.components.PersistentStateComponent
-import com.intellij.openapi.components.ServiceManager
 import com.intellij.openapi.components.State
 import com.intellij.openapi.components.Storage
 import com.intellij.util.xmlb.XmlSerializerUtil
@@ -34,6 +34,6 @@ class AppSettingsState : PersistentStateComponent<AppSettingsState?> {
 
     companion object {
         val instance: AppSettingsState
-            get() = ServiceManager.getService(AppSettingsState::class.java)
+            get() = ApplicationManager.getApplication().getService(AppSettingsState::class.java)
     }
 }

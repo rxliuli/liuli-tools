9fdde92ca914c72e3f047b50b0653d47788521fe
rxliuli <rxliuli@gmail.com>
2021-12-30 10:44:24 +0800

    feat: 实现 provide/inject

--- libs/mobx-vue3-hooks/src/index.ts
+++ libs/mobx-vue3-hooks/src/index.ts
@@ -32,4 +32,26 @@ export function useVWatchEffect(fn: () => void): void {
   useEffect(() => autorun(fn), [])
 }
 
+const contextMap = observable(new Map<ContextKey<any>, any>())
+// eslint-disable-next-line
+export interface ContextKey<T> extends Symbol {}
+export function useVProvide<T>(key: ContextKey<T>, value: T): void {
+  useState(action(() => contextMap.set(key, value)))
+  useEffect(
+    action(() => {
+      contextMap.set(key, value)
+      return action(() => {
+        contextMap.delete(key)
+      })
+    }),
+    [],
+  )
+}
+export function useVInject<T>(key: ContextKey<T>): T | undefined {
+  const value = useMemo(() => computed(() => contextMap.get(key)), [])
+  const state = useVRef(value.get())
+  useVWatchEffect(() => (state.value = value.get()))
+  return state.value
+}
+
 export const defineComponent = observer

336cc8908d47e45bbcab13affe42f6d6333b8eb1
rxliuli <rxliuli@gmail.com>
2022-01-03 21:55:32 +0800

    feat: 添加 @liuli-util/react-router 的两个示例项目

---
+++ examples/react-router-nest-example/src/views/Layout.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react'
+import { RouterView } from '@liuli-util/react-router'
+
+export const Layout: React.FC = () => (
+  <div>
+    <h2>Layout Header</h2>
+    <RouterView />
+  </div>
+)

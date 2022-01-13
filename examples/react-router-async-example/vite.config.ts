c95402935c5aa59e0572d3ddfc1d0dabb3ed407a
rxliuli <rxliuli@gmail.com>
2022-01-13 21:14:06 +0800

    feat: 实现插件 rollup-plugin-ts-alias

--- examples/react-router-async-example/vite.config.ts
+++ examples/react-router-async-example/vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { tsAlias } from '@liuli-util/rollup-plugin-ts-alias/dist'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react(), tsAlias(['@liuli-util/'])],
 })

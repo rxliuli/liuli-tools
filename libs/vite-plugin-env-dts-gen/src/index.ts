b1b7536129fef4dd8ca3788bd0a95c7cb39b71bf
rxliuli <rxliuli@gmail.com>
2022-02-12 22:47:09 +0800

    fix: 修复 vite-env 插件没有兼容 root 配置的错误
    ref: https://github.com/rxliuli/liuli-tools/issues/18

--- libs/vite-plugin-env-dts-gen/src/index.ts
+++ libs/vite-plugin-env-dts-gen/src/index.ts
@@ -3,22 +3,26 @@ import { gen } from './gen'
 import * as path from 'path'
 
 export function envDtsGen(): Plugin {
+  let rootPath: string
   return {
     name: 'vite-plugin-env-dts-gen',
+    configResolved(resolveConfig) {
+      rootPath = resolveConfig.root
+    },
     configureServer(server) {
       server.watcher.add('.env*')
       const listener = async (filePath: string) => {
-        const relative = path.relative(path.resolve(), filePath)
+        const relative = path.relative(rootPath, filePath)
         // console.log('filePath: ', relative)
         if (relative.startsWith('.env')) {
-          await gen(path.resolve())
+          await gen(rootPath)
         }
       }
       server.watcher.on('change', listener)
       server.watcher.on('add', listener)
     },
     async buildStart() {
-      await gen(path.resolve())
+      await gen(rootPath)
     },
   }
 }

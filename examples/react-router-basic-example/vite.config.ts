3ae21239c2b9467df5289d1372b86e0fb120f729
rxliuli <rxliuli@gmail.com>
2022-10-02 16:18:21 +0800

    feat: 更新 react-router 使用相关示例，添加 tsAlias 插件

--- examples/react-router-basic-example/vite.config.ts
+++ examples/react-router-basic-example/vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { tsAlias } from '@liuli-util/rollup-plugin-ts-alias'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsAlias({ includes: ['@liuli-util/'] }) as any],
 })
--- examples/react-router-redirect-example/src/constants/router.tsx
+++ examples/react-router-redirect-example/src/constants/router.tsx
@@ -1,9 +1,8 @@
 import { RouteConfig, createHashHistory } from '@liuli-util/react-router'
-import { Navigate } from 'react-router-dom'
 import HelloWorld from '../views/HelloWorld'
 
 export const routeList: RouteConfig[] = [
-  { path: '/', component: () => <Navigate replace={true} to={'/hello-world'} /> },
+  { path: '/', redirect: '/hello-world' },
   { path: '/hello-world', component: HelloWorld },
 ]
 

b2aaf7c802b0a70cdb626bee5d9a34a9435d4ffa
rxliuli <rxliuli@gmail.com>
2023-07-02 17:48:26 +0800

    feat: 正确转换 background 字段

--- packages/create-liuli/packages/react-chrome-plugin/src/main.tsx
+++ packages/create-liuli/packages/react-chrome-plugin/src/main.tsx
@@ -4,7 +4,7 @@ import App from './App.tsx'
 import './index.css'
 import { initI18n } from './constants/i18n.ts'
 
-// await initI18n()
+await initI18n()
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
--- packages/vite-plugin-firefox-dist/src/index.ts
+++ packages/vite-plugin-firefox-dist/src/index.ts
@@ -5,6 +5,11 @@ import { cp, readFile, rm, writeFile } from 'node:fs/promises'
 import { Manifest } from 'webextension-polyfill'
 import { DeepPartial } from 'utility-types'
 
+/**
+ * 将构建的 dist/ 目录中的 chrome 插件复制到 dist-firefox 中生成 firefox 插件
+ * @param manifest 需要合并的 manifest 对象
+ * @returns
+ */
 export function firefox(manifest: DeepPartial<Manifest.WebExtensionManifest>): Plugin {
   return {
     name: 'vite-plugin-firefox-dist',
@@ -16,7 +21,15 @@ export function firefox(manifest: DeepPartial<Manifest.WebExtensionManifest>): P
         force: true,
       })
       const jsonPath = path.resolve(firefoxDist, 'manifest.json')
-      await writeFile(jsonPath, JSON.stringify(merge(JSON.parse(await readFile(jsonPath, 'utf-8')), manifest), null, 2))
+      const json = JSON.parse(await readFile(jsonPath, 'utf-8')) as Manifest.WebExtensionManifest
+      // 需要转换 background 字段，参考: https://github.com/mozilla/web-ext/issues/2532
+      if (json.background && 'service_worker' in json.background) {
+        json.background = {
+          scripts: [json.background.service_worker],
+          type: json.background.type,
+        }
+      }
+      await writeFile(jsonPath, JSON.stringify(merge(json, manifest), null, 2))
     },
     apply: 'build',
   }

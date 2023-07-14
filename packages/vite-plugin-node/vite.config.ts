e61812c10e2ca213cdb472c4abf0d08ed190f17f
rxliuli <rxliuli@gmail.com>
2023-07-14 23:35:57 +0800

    chore: use dts bundle config

--- packages/vite-plugin-node/vite.config.ts
+++ packages/vite-plugin-node/vite.config.ts
@@ -2,12 +2,5 @@ import { defineConfig } from 'vite'
 import { node } from './src'
 
 export default defineConfig({
-  plugins: [
-    node({
-      dts: true,
-    }),
-  ],
-  build: {
-    minify: false,
-  },
+  plugins: [node({ dts: { bundle: true } })],
 })
